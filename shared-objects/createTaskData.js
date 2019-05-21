7ad83032f74ac8fac57021ad076d27c666661025
Maxim <maxim.renz@gmail.com>
2019-05-21 16:12:30 +0200

    Create-Task-Test implemented

---
+++ shared-objects/createTaskData.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    elem: {
+        sidebarTaskButton: "a[data-testid='Aufgaben']",
+        createTaskButton: "a[data-testid='CreateTaskButton']",
+        taskNameField: "input[data-testid='TaskName']",
+        submitTaskButton: "button[data-testid='SubmitTaskButton']",
+        taskCardTitle: "h5[data-testid='taskTitle']"
+    }
+
+};
