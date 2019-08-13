87bebc39410fd57205602b2023e5bbb6b01957c4
Adrian Jost <adrianjost@users.noreply.github.com>
2019-08-13 14:08:21 +0200

    removed more legacy code

--- shared-objects/servers.js
+++ shared-objects/servers.js
@@ -5,12 +5,11 @@ const config = {
   CLIENT: {
     PROTOCOLL: process.env.IT_CLIENT_PROTOCOLL || 'http',
     HOST: process.env.IT_CLIENT_HOST || 'localhost',
-    PORT: process.env.IT_CLIENT_PORT || '3100'
+    PORT: process.env.IT_CLIENT_PORT || '3100',
+    get URL() {
+      return `${config.CLIENT.PROTOCOLL}://${config.CLIENT.HOST}:${config.CLIENT.PORT}`
+    }
   }
 };
-
-// ADD AGGREGATED VALUE
-config.CLIENT.URL = `${config.CLIENT.PROTOCOLL}://${config.CLIENT.HOST}:${config.CLIENT.PORT}`;
-
 
 module.exports = config;
