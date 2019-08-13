f245c096ab989672662cb41abbcb8635bf7b3c81
Adrian Jost <adrianjost@users.noreply.github.com>
2019-08-13 13:56:34 +0200

    Update servers.js

--- shared-objects/servers.js
+++ shared-objects/servers.js
@@ -1,12 +1,16 @@
 'use strict';
 
+// STATIC DATA FROM ENV + DEFAULTS
 const config = {
   CLIENT: {
+    PROTOCOLL: process.env.IT_CLIENT_PROTOCOLL || 'http',
     HOST: process.env.IT_CLIENT_HOST || 'localhost',
     PORT: process.env.IT_CLIENT_PORT || '3100'
   }
 };
 
-config.CLIENT.URL = `${config.CLIENT.HOST}:${config.CLIENT.PORT}`;
+// ADD AGGREGATED VALUE
+config.CLIENT.URL = `${config.CLIENT.PROTOCOLL}://${config.CLIENT.HOST}:${config.CLIENT.PORT}`;
+
 
 module.exports = config;
