88827fbb7cad5b899ee5241373fdc062a1192cf6
Adrian Jost <adrianjost@users.noreply.github.com>
2019-08-13 13:20:54 +0200

    remove legacy code

--- shared-objects/servers.js
+++ shared-objects/servers.js
@@ -2,11 +2,11 @@
 
 const config = {
   CLIENT: {
-    HOST: process.env.IT_CLIENT_HOST || "localhost",
-    PORT: process.env.IT_CLIENT_PORT || "3100",
+    HOST: process.env.IT_CLIENT_HOST || 'localhost',
+    PORT: process.env.IT_CLIENT_PORT || '3100'
   }
-}
+};
 
-config.CLIENT.URL = `${config.CLIENT.HOST}:${config.CLIENT.PORT}`
+config.CLIENT.URL = `${config.CLIENT.HOST}:${config.CLIENT.PORT}`;
 
 module.exports = config;
