2bb6ca885a0900632d0daf1c73553e9c5101cf41
yuliashikh <yulia.shikhareva@gmail.com>
2019-09-16 11:48:21 +0200

    create Team: edited

--- page-objects/createTeam.js
+++ page-objects/createTeam.js
@@ -1,13 +1,15 @@
 'use strict';
 
-
 const teamData = require('../shared-objects/teamsData');
 const { expect } = require('chai');
 let name;
 let indexes = [];
+var searchBox;
+var numberOFmatches;
+let pupilNames;
 
 module.exports = {
-    createATeam: async function(fullname) {
+    createATeamSTEPS: async function() {
         let url = teamData.addTeamURL;
         await helpers.loadPage(url, 20);
         name= "a team";
@@ -15,8 +17,23 @@ module.exports = {
         await nameField.setValue(name);
         let createBtn = await driver.$(teamData.createTeamBtn);
         await createBtn.click();
-        await this.addTeamMembers(fullname);
     },
+    addMembersToTheTeamSTEPS: async function() {
+        await driver.execute('document.querySelector("#team_settings").click()');
+        await driver.$('#team_settings').then(button=>button.click());
+        await driver.execute('document.querySelector("#administrate_team_members").click()');
+        await driver.execute('document.querySelector("#internal_team_members").click()');
+        await driver.pause(2000);
+        let container = await driver.$('#userIds___chosen > ul');
+        searchBox = await container.$('li > input');
+        await searchBox.click();
+        let arrayNumberOFmatches = await driver.$$(".chosen-results li");
+        numberOFmatches = arrayNumberOFmatches.length;
+        let pupils = await driver.$$('#userIds___chosen > div > ul > li');
+        let promiseNames = pupils.map(async element => await element.getText());
+        pupilNames = await Promise.all(promiseNames);
+    },
+    
     getTeamNames: async function() {
         let teamsPage = teamData.url;
         await helpers.loadPage(teamsPage, 20);
@@ -24,36 +41,34 @@ module.exports = {
         const namePromises = elements.map(async element => await element.getText());
         const teamNames = await Promise.all(namePromises);
         return teamNames;
-    },
-    addTeamMembers: async function(fullname) {
-        await driver.execute('document.querySelector("#team_settings").click()');
-        await driver.$('#team_settings').then(button=>button.click());
-        await driver.execute('document.querySelector("#administrate_team_members").click()');
-        await driver.execute('document.querySelector("#internal_team_members").click()');
-        await driver.pause(2000);
-        let container = await driver.$('#userIds___chosen > ul');
-        const searchBox = await container.$('li > input');
-        await searchBox.click();
 
-        let arrayNumberOFmatches = await driver.$$(".chosen-results li");
-        let numberOFmatches = arrayNumberOFmatches.length;
-        let pupils = await driver.$$('#userIds___chosen > div > ul > li');
-        let promiseNames = pupils.map(async element => await element.getText());
-        const pupilNames = await Promise.all(promiseNames);
+    },
+    addTeamMemberSTEPS: async function(fullname) {
+        await searchBox.click();
         for (var i=0; i<=numberOFmatches; i++) {
-            if (pupilNames[i]=="Paula Meyer") {
-                await indexes.push(i+1);
-            }
-            else if (pupilNames[i]==fullname) {
+            if (pupilNames[i]==fullname) {
                 await indexes.push(i+1);
             }
         }
-        let chooseTeammember1 = await driver.$('#userIds___chosen > div > ul > li:nth-child('+indexes[0]+')');
-        await chooseTeammember1.click();
+    },
+        addTeamMemberOne: async function() {
+            let chooseTeammember1 = await driver.$('#userIds___chosen > div > ul > li:nth-child('+indexes[0]+')');
+            await chooseTeammember1.click();
+        },
+        addTeamMemberTwo: async function() {
+            let chooseTeammember1 = await driver.$('#userIds___chosen > div > ul > li:nth-child('+indexes[1]+')');
+            await chooseTeammember1.click();
+        },
+       
+    createTeamWithTwoMembers: async function(nameOne, nameTwo) {
+        await this.createATeamSTEPS();
+        await this.addMembersToTheTeamSTEPS();
+        await this.addTeamMemberSTEPS(nameOne);
+        await this.addTeamMemberOne();
         await searchBox.click();
-        let chooseTeammember2 = await driver.$('#userIds___chosen > div > ul > li:nth-child('+indexes[1]+')');
-        await chooseTeammember2.click();
-      },
+        await this.addTeamMemberSTEPS(nameTwo);
+        await this.addTeamMemberTwo();
+    },
     verify: async function() {
         let url = teamData.url;
         await helpers.loadPage(url, 20);
--- step_definitions/createTeam-steps.js
+++ step_definitions/createTeam-steps.js
@@ -6,6 +6,7 @@ let teacherLogin = require('../page-objects/teacherLogin');
 let newPupil = require('../page-objects/administration');
 const Login = require('../shared-objects/loginData');
 let fullname;
+let ownPupil = "Paula Meyer"; // depends on teacher!
 
 
 Given(/^the teacher starts on the login page and$/, function() {
@@ -13,17 +14,19 @@ Given(/^the teacher starts on the login page and$/, function() {
     return helpers.loadPage(url, 10);
   });
 Given(/^teacher successfully logs in and$/, function() {
-    return teacherLogin.performLogin(Login.deafultTeacherUsername,Login.defaultTeacherpassword);
+    return teacherLogin.performLogin(Login.defaultTeacherUsername,Login.defaultTeacherpassword);
   });
 Given(/^the teacher creates a new pupil$/, function() {
     let firstname = "New";
     let lastname= "Team Member";
-    fullname = firstname+" "+ lastname;
     let email = "newTeamMember@schul-cloud.org";
+    fullname = firstname + " " + lastname;
     return newPupil.createNewPupil(firstname, lastname, email);
   });
 When(/^teacher creates a new team$/, function() {
-    return createTeam.createATeam(fullname);
+  let nameOne = fullname;
+  let nameTwo = ownPupil;
+  return createTeam.createTeamWithTwoMembers(nameOne, nameTwo)
   });
 Then(/^this team should be displayed on the team page$/, function() {
     return createTeam.verify();
