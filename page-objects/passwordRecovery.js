390892907b1dcb3027f77d935a45801d2b21b783
yuliashikh <yulia.shikhareva@gmail.com>
2020-01-14 14:35:25 +0100

    password recovery
    checks whether the system sends a mail with password recovery link

--- page-objects/passwordRecovery.js
+++ page-objects/passwordRecovery.js
@@ -1,6 +1,14 @@
 'use strict';
+const { SERVER } = require("../shared-objects/servers");
+const jwtDecode = require("jwt-decode");
 const mailHelper = require('../shared_steps/mailslurper');
+const loginData = require('../shared-objects/loginData');
+const teacherLogin = require('../page-objects/teacherLogin');
+const firstLogin = require('../shared_steps/firstLogin.js');
+const Axios = require("axios");
 
+
+// TODO: mail lookup
 
 module.exports = {
 
@@ -19,17 +27,61 @@ submitEmail: async function(registeredEmail) {
     await submitBtn.click();
     
 },
-verifySendMail: async function(email) {
+/* the func checks whether mail with password was sent */
+isMailed: async function(email) {
     const subjectOfEmail = await mailHelper.getEmailSubject(email)
     const expectedSubject = "Passwort zurücksetzen für die Schul-Cloud";
     await expect(subjectOfEmail).to.equal(expectedSubject);
 },
-userCanLogIn: async function(email) {
-    const mailedLink = await mailHelper.getEmailLink(email);
-    await helpers.loadPage(mailedLink, 20); 
-
+/* get the link from mailslurper, navigate and set a new valid password */ 
+setNewPassword: async function(email, password) { 
+    const link = await driver.getUrl(); 
+    const regExp = /\/\/[a-z],[0-9]{4}/;
+    const url = await link.match(regExp); 
+    const sys = await url.substr(2, url.length-1);
+    const mailedLink = await mailHelper.getEmailLink(email, sys);
+    await helpers.loadPage(mailedLink, 20);
+    await this.setNewPasswordSteps(password);
 }, 
+setNewPasswordSteps: async function(password) {
+    let passwordSelector = await driver.$('#password');
+    let passwordControlSelector = await driver.$('#password_control'); 
+    await helpers.waitAndSetValue(passwordSelector, password);
+    await helpers.waitAndSetValue(passwordControlSelector, password); 
+    let submitBtn = await driver.$('input[type="submit"]');
+    await helpers.waitAndClick(submitBtn); 
 
-
-
+},
+/* a helper function which returns the role of the person */ 
+getUsersRole: async function() {
+    const cookies = await driver.getCookies(["jwt"])
+    const jwt = cookies[0].value;
+    const userInfo = jwtDecode(jwt);
+    const years = await Axios.request({
+            url: `${SERVER.URL}/me`,
+            headers: {
+                Cookie: `jwt=Bearer ${jwt};`
+            }
+        })
+    const myRole = roles.name;
+    return myRole;
+},
+userCanLoginWithANewPassword: async function(email, password) {
+    await helpers.loadPage(loginData.url, 10);
+    await driver.pause(1000);
+    await teacherLogin.performLogin(email, password); // this function is also applicable to student and admin login
+    let usersRole = await this.getUsersRole; // then we have to perform the first login accordingly 
+    switch (usersRole) {
+        case "teacher" :
+        await firstLogin.teacherLogin(email, password);
+        break;
+        case "student":
+        await firstLogin.firstLoginPupilFullAge(email, password);
+        break;
+        case "admin":
+        await firstLogin.teacherLogin(email, password);
+        break;
+    }
+    await teacherLogin.loginResult(); // verify that the initials in the circle correspond the name of the person
+}
 }
--- shared_steps/mailslurper.js
+++ shared_steps/mailslurper.js
@@ -13,18 +13,27 @@ const getEmailSubject = (email) => request(options)
         var data = response.mailItems;
         var object = data.filter(d => d.toAddresses.includes(email))[0];
         const emailSubject = object.subject;
-        //let re = /PIN: (\d\d\d\d)/g;
         return emailSubject;
     });
-const getEmailLink = (email) => request(options)
+const getEmailLink = (email, sys) => request(options)
     .then(function(response) {
         var data = response.mailItems;
         var object = data.filter(d => d.toAddresses.includes(email))[0];
         const emailSubject = object.body;
+        switch (sys) {
+            case "localhost": 
+            var regExp = /localhost:.*\/pwrecovery\/[a-z, 0-9, A-Z]{24}/;
+            break;
+            case staging: 
+            var regExp = /staging:.*\/pwrecovery\/[a-z, 0-9, A-Z]{24}/;
+            break;
+            case live: 
+            var regExp = /https:.*\/pwrecovery\/[a-z, 0-9, A-Z]{24}/;
+            break;
+        }
+    return emailSubject.match(regExp);
 
-
-    },
-    
+    });
 
 
 module.exports = {
