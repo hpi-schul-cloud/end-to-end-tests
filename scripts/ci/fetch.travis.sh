b3a287ebbaea147c7ced14ec8d46aa4fb27aa08f
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-02-10 14:27:44 +0100

    Create fetch.travis.sh

---
+++ scripts/ci/fetch.travis.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+export BRANCH_NAME=${TRAVIS_PULL_REQUEST_BRANCH:=$TRAVIS_BRANCH}
+
+echo "BRANCH: $BRANCH_NAME"
+
+# fetch default (develop) script
+echo "try fetching script from default branch"
+curl -f "https://raw.githubusercontent.com/schul-cloud/integration-tests/develop/scripts/ci/integration-test.travis.sh" > integration-test.sh || true
+# use master as default for releases & hotfixes
+if [[ $BRANCH_NAME = release* || $BRANCH_NAME = hotfix* ]];
+then
+  echo "try fetching script from master branch"
+  curl -f "https://raw.githubusercontent.com/schul-cloud/integration-tests/master/scripts/ci/integration-test.travis.sh" > integration-test.sh || true
+fi
+# use branch specific script if available
+echo "try fetching script from $BRANCH_NAME branch"
+curl -f "https://raw.githubusercontent.com/schul-cloud/integration-tests/$BRANCH_NAME/scripts/ci/integration-test.travis.sh" > integration-test.sh || true
+
+ls -a
+chmod 700 integration-test.sh
+bash integration-test.sh
