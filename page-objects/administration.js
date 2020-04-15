248af2a875a4311bfbbdc7683e3dc6d973152cdf
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-04-15 13:21:07 +0200

    fix @createTeam

--- page-objects/administration.js
+++ page-objects/administration.js
@@ -1,77 +1,94 @@
-'use strict';
-const firstLogin = require('../shared_steps/firstLogin.js');
-const { expect } = require('chai');
-const Admin = require('../shared-objects/administrationData');
+"use strict";
+const firstLogin = require("../shared_steps/firstLogin.js");
+const { expect } = require("chai");
+const Admin = require("../shared-objects/administrationData");
 var length;
 let oldPassword;
-let eMAIL;
+let tmpEMail;
 let name;
-let newPassword = "Schulcloud1!"
+let newPassword = "Schulcloud1!";
 
 module.exports = {
-goToAdministration: function() {
-    let url = Admin.urlAdministration;
-    return helpers.loadPage(url, 10);
-},
-createNewPupil: async function(firstname, lastname, email) {
-    name=firstname;
-    eMAIL = email;
-    await this.goToAdministration();
-    let administrateStudentsBtn = await driver.$(Admin.administrateStudentsBtn);
-    await administrateStudentsBtn.click();
-    let addBtn = await driver.$(Admin.addStudentBtn);
-    await addBtn.click();
-    await driver.pause(1000);
-    let firstName= await driver.$(Admin.setFirstName);
-    await firstName.setValue(firstname);
-    let secondName = await driver.$(Admin.setLastName);
-    await secondName.setValue(lastname);
-    let eMail = await driver.$(Admin.setEmail);
-    await eMail.setValue(email);
-    await this.executeScript();
-    let sendAMessageBox = await driver.$(Admin.sendALinkBox);
-    await sendAMessageBox.click();
-    let addButton = await driver.$('body > div.modal.fade.add-modal.in > div > div > form > div.modal-footer > button.btn.btn-primary.btn-submit');
-    await addButton.click();
-},
-executeScript: async function() {
-    await driver.pause(1500);
-    await driver.execute('document.querySelector("#create_birthday").value = "13.08.1990"')
-},
-emailsOfThePupils: async function() {
-    let names = await driver.$$(Admin.namesContainer + ' > tr');
-    return Promise.all(names.map(async (nameContainer) => {
-        const emailContainer = await nameContainer.$("td:nth-child(3)");
-        return await emailContainer.getText();
-    }))
-},
-verify: async function(email) {
-    let emails = await this.emailsOfThePupils();
-    await expect(emails).to.contain(email);
-},
-submitConsent: async function(e_mail) {
-    let names = await driver.$$(Admin.namesContainer + ' > tr');
-    length = names.length; 
-    for (var i = 1; i<= length; i++) {
-        let emailPromise =  await driver.$(Admin.namesContainer + ' > tr:nth-child('+i+') > td:nth-child(3)');
-        let email = await emailPromise.getText();
-        if (email===e_mail){
-            let boxConsent = await driver.$(Admin.namesContainer + ' > tr:nth-child('+i+') > td:nth-child(7) > a:nth-child(2) > i');
-            await boxConsent.click();
-            let submitBtn = await driver.$(Admin.consentSubmitBtn);
-            let passwordField = await driver.$('#passwd');
-            let password_old = await passwordField.getValue();
-            oldPassword = password_old;
-            await submitBtn.click();
-            break;
-        }
-    }
-},
-newPupilLogsIn: async function() {
-    await firstLogin.logout();
-    await firstLogin.pupilLogin(eMAIL, oldPassword);
-},
-pupilAcceptsDataProtection: async function() {
-    await firstLogin.firstLoginPupilFullAge(name, newPassword);
-}
-}
+	goToAdministration: function () {
+		let url = Admin.urlAdministration;
+		return helpers.loadPage(url, 10);
+	},
+	createNewPupil: async function (firstname, lastname, email) {
+		name = firstname;
+		tmpEMail = email;
+		await this.goToAdministration();
+		// navigate to students administration page
+		let administrateStudentsBtn = await driver.$(Admin.administrateStudentsBtn);
+		await administrateStudentsBtn.click();
+		// open fab and click add Student
+		const mainFAB = await driver.$(`.fab-wrapper > button`);
+		await mainFAB.click();
+		await driver.pause(500); // wait for fab to open
+		const btnAddStudent = await driver.$(
+			`a[href="/administration/students/new"]`
+		);
+		await btnAddStudent.click();
+		await driver.pause(3000); // wait for page load TODO: wait for actual page load
+		// fill userdata
+		let firstName = await driver.$(Admin.setFirstName);
+		await firstName.setValue(firstname);
+		let secondName = await driver.$(Admin.setLastName);
+		await secondName.setValue(lastname);
+		let eMail = await driver.$(Admin.setEmail);
+		await eMail.setValue(email);
+		await this.executeScript(
+			`document.querySelector("${Admin.birthday_field}").value = "1990-08-13"`
+		);
+		let addButton = await driver.$(Admin.submitBtn);
+		await addButton.click();
+	},
+	executeScript: async function (script) {
+		await driver.pause(1500);
+		await driver.execute(script);
+	},
+	emailsOfThePupils: async function () {
+		let names = await driver.$$(Admin.namesContainer + " > tr");
+		return Promise.all(
+			names.map(async (nameContainer) => {
+				const emailContainer = await nameContainer.$("td:nth-child(3)");
+				return await emailContainer.getText();
+			})
+		);
+	},
+	verify: async function (email) {
+		let emails = await this.emailsOfThePupils();
+		await expect(emails).to.contain(email);
+	},
+	submitConsent: async function (e_mail) {
+		let names = await driver.$$(Admin.namesContainer + " > tr");
+		length = names.length;
+		for (var i = 1; i <= length; i++) {
+			let emailPromise = await driver.$(
+				Admin.namesContainer + " > tr:nth-child(" + i + ") > td:nth-child(3)"
+			);
+			let email = await emailPromise.getText();
+			if (email === e_mail) {
+				let boxConsent = await driver.$(
+					Admin.namesContainer +
+						" > tr:nth-child(" +
+						i +
+						") > td:nth-child(7) > a:nth-child(2) > i"
+				);
+				await boxConsent.click();
+				let submitBtn = await driver.$(Admin.consentSubmitBtn);
+				let passwordField = await driver.$("#passwd");
+				let password_old = await passwordField.getValue();
+				oldPassword = password_old;
+				await submitBtn.click();
+				break;
+			}
+		}
+	},
+	newPupilLogsIn: async function () {
+		await firstLogin.logout();
+		await firstLogin.pupilLogin(tmpEMail, oldPassword);
+	},
+	pupilAcceptsDataProtection: async function () {
+		await firstLogin.firstLoginPupilFullAge(name, newPassword);
+	},
+};
--- step_definitions/createTeam-steps.js
+++ step_definitions/createTeam-steps.js
@@ -1,42 +1,43 @@
-'use strict';
+"use strict";
 
-let loginData = require('../shared-objects/loginData');
-const createTeam = require('../page-objects/createTeam');
-let teacherLogin = require('../page-objects/teacherLogin');
-let newPupil = require('../page-objects/administration');
-const Login = require('../shared-objects/loginData');
-let fullname1;
-let fullname2;
-let teamName;
+let loginData = require("../shared-objects/loginData");
+const createTeam = require("../page-objects/createTeam");
+let teacherLogin = require("../page-objects/teacherLogin");
+let newPupil = require("../page-objects/administration");
+const Login = require("../shared-objects/loginData");
 
+const addededStudents = [];
 
+Given(/^the teacher started on the login page and$/, function () {
+	let url = loginData.url;
+	return helpers.loadPage(url, 10);
+});
+Given(/^teacher successfully logged in$/, function () {
+	return teacherLogin.performLogin(
+		Login.defaultTeacherUsername,
+		Login.defaultTeacherpassword
+	);
+});
+When(/^teacher adds a new student with (.*), (.*), (.*)$/, function (
+	firstname,
+	lastname,
+	email
+) {
+	const fullname = firstname + " " + lastname;
+	addededStudents.push(fullname);
+	return newPupil.createNewPupil(firstname, lastname, email);
+});
 
-Given(/^the teacher started on the login page and$/, function() {
-    let url = loginData.url;
-    return helpers.loadPage(url, 10);
-  });
-Given(/^teacher successfully logged in$/, function() {
-    return teacherLogin.performLogin(Login.defaultTeacherUsername,Login.defaultTeacherpassword);
-  });
-  When(/^teacher adds a new student with (.*), (.*), (.*)$/,  function(firstname1, lastname1, email1) {
-    fullname1 = firstname1+" "+lastname1;
-    return newPupil.createNewPupil(firstname1, lastname1, email1);
-  });
-  When(/^teacher adds one more student with (.*), (.*), (.*)$/,  function(firstname2, lastname2, email2) {
-    fullname2 = firstname2+" "+lastname2;
-    return newPupil.createNewPupil(firstname2, lastname2, email2);
-  });
+When(/^teacher creates a new team with (.*) and$/, function (teamname) {
+	return createTeam.createTeamAndGoToSettings(teamname);
+});
+When(/^teacher adds two students to this team$/, function () {
+	return createTeam.addTwoTeamMemebers(...addededStudents);
+});
 
-  When(/^teacher creates a new team with (.*) and$/,  function(teamname) {
-    teamName = teamname;
-    return createTeam.createTeamAndGoToSettings(teamname);
-  });
-When(/^teacher adds two students to this team$/, function() {
-  return createTeam.addTwoTeamMemebers(fullname1, fullname2)
-
-  });
-
-Then(/^this team should be displayed on the team page$/, async function() {
-    let teamNames = await createTeam.getTeamNames();
-    await expect(teamNames).to.include(teamName);
-  });
+Then(/^the team (.*) should be displayed on the team page$/, async function (
+	teamName
+) {
+	let teamNames = await createTeam.getTeamNames();
+	await expect(teamNames).to.include(teamName);
+});
