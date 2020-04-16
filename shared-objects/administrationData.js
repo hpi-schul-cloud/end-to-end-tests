d7ceb0dd070b116c8146722161d9fb19d148c57d
Adrian Jost <22987140+adrianjost@users.noreply.github.com>
2020-04-16 11:29:47 +0200

    feature/new admin routes (#57)
    * fix @createTeam
    
    * fix @createNewPupil (kind of)
    
    * remove workaround that is not needed anymore

--- shared-objects/administrationData.js
+++ shared-objects/administrationData.js
@@ -1,18 +1,18 @@
 const { CLIENT } = require("./servers");
 
 module.exports = {
-  urlAdministration: `${CLIENT.URL}/administration`,
-  submitBtn: 'button[type=\'submit\']',
-  administrateStudentsBtn: 'div[data-testid=\'administrate_students\']',
-  administrateTeachersBtn: 'div[data-testid=\'administrate_teachers\']',
-  administrateClassesBtn:'div[data-testid=\'administrate_classes\']',
-  addStudentBtn:'button[data-testid=\'btn_add_student\']',
-  setFirstName:'input[id=\'create_firstname\']',
-  setFirstName:'input[data-testid=\'create_student_input_firstname\']',
-  setLastName:'input[data-testid=\'create_student_input_lastname\']',
-  setEmail:'input[data-testid=\'create_student_input_email\']',
-  sendALinkBox:'input[data-testid=\'create_student_input_send_link\']',
-  namesContainer: 'tbody[data-testid=\'students_names_container\']',
-  consentSubmitBtn: 'button[data-testid=\'submit_consent\']',
-  birthday_field: 'input[data-testid=\'create_birthday\']'
-}
+	urlAdministration: `${CLIENT.URL}/administration`,
+	submitBtn: "button[type='submit']",
+	administrateStudentsBtn: "div[data-testid='administrate_students']",
+	administrateTeachersBtn: "div[data-testid='administrate_teachers']",
+	administrateClassesBtn: "div[data-testid='administrate_classes']",
+	addStudentBtn: "button[data-testid='btn_add_student']",
+	setFirstName: "input[data-testid='input_create-user_firstname']",
+	setLastName: "input[data-testid='input_create-user_lastname']",
+	setEmail: "input[data-testid='input_create-user_email']",
+	sendALinkBox: "input[data-testid='input_create-student_send-registration']",
+	namesContainer: "main table tbody",
+	consentSubmitBtn: "button[data-testid='submit_consent']",
+	birthday_field: "[data-testid='input_create-student_birthdate'] input",
+	submitBtn: "button[data-testid='button_create-user_submit']",
+};
