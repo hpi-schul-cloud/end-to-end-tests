a515fa77eea01e059ef5f647008f970b4fcc1a5b
Paweł Choniawko <pawel.choniawko@gmail.com>
2020-06-02 12:53:21 +0200

    fix test news

--- page-objects/copyCourse.js
+++ page-objects/copyCourse.js
@@ -39,7 +39,7 @@ module.exports = {
 	cloneCourse: async function() {
 		let settingsBtn =  ".fa.fa-cog.i-cog";
 		await helpers.waitAndClick(settingsBtn);
-		let copyCourseBtn = await driver.$('div.dropdown.dropdown-course.minimal-button.open > div > a:nth-child(4)') ;
+		let copyCourseBtn = "div.dropdown.dropdown-course.minimal-button > div.show > a:nth-child(4)";
 		await helpers.waitAndClick(copyCourseBtn);
 		await driver.pause(1500);
 		let submitBtn= "button.btn.btn-primary.btn-submit";
--- runtime/helpers.js
+++ runtime/helpers.js
@@ -311,17 +311,12 @@ module.exports = {
 	},
 
 	waitAndClick: async function (selector) {
-		try {
-			let elem = await driver.$(selector);
-			await elem.waitForDisplayed(DELAY_3_SECOND);
-			await elem.waitForEnabled(DELAY_1_SECOND);
-			await elem.click();
-			await driver.pause(DELAY_500_MILLISECOND);
-		}
-		catch (err) {
-			log.error(err.message);
-			throw err;
-		}
+		let elem = await driver.$(selector);
+		await elem.waitForDisplayed(DELAY_3_SECOND);
+		await elem.waitForEnabled(DELAY_1_SECOND);
+		await elem.click();
+		await driver.pause(DELAY_500_MILLISECOND);
+	
 	},
 
 	waitAndSetValue: async function (selector, value) {
