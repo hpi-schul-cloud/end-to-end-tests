460121335b801c92f27a4923758e824068396fa2
hpalac <hanna.palac@capgemini.com>
2020-06-09 07:56:21 +0200

    SC-3838: reverting changes for copy course so the tests pass on travis

--- page-objects/copyCourse.js
+++ page-objects/copyCourse.js
@@ -39,7 +39,7 @@ module.exports = {
 	cloneCourse: async function() {
 		let settingsBtn =  ".fa.fa-cog.i-cog";
 		await helpers.waitAndClick(settingsBtn);
-		let copyCourseBtn = "div.dropdown.dropdown-course.minimal-button > div.dropdown-menu.dropdown-menu-course > a:nth-child(4)";
+		let copyCourseBtn = "div.dropdown.dropdown-course.minimal-button > div.show > a:nth-child(4)";
 		await helpers.waitAndClick(copyCourseBtn);
 		await driver.pause(1500);
 		let submitBtn= "button.btn.btn-primary.btn-submit";
