095a90b0692004583918a21f79d846ce5467075a
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-06-30 09:06:08 +0200

    new branch from pull 83

--- page-objects/administration.js
+++ page-objects/administration.js
@@ -15,6 +15,46 @@ goToAdministration: function() {
     let url = Admin.urlAdministration;
     return helpers.loadPage(url, 10);
 },
+createNewClass: async function (className = '11c') {
+    // navigates to administration tools
+    await this.goToAdministration();
+
+    // navigates to class administration
+    const administrateClassesBtn = await driver.$(Admin.administrateClassesBtn);
+    await administrateClassesBtn.click();
+
+    const pageTitle = await driver.getTitle()
+    expect(pageTitle.startsWith('Administration: Klassen')).to.equal(true)
+
+    const createClassBtn = await driver.$(Admin.classCreateBtn);
+    await createClassBtn.click();
+
+    const pageTitle2 = await driver.getTitle()
+    expect(pageTitle2.startsWith('Erstelle eine neue Klasse')).to.equal(true)
+
+    const classCreationExtraOptions = await driver.$(Admin.classCreationExtraOptions)
+    await classCreationExtraOptions.click()
+
+    const classNameInputField = await driver.$(Admin.classNameInputField)
+    await classNameInputField.setValue(className);
+
+    const confirmButton = await driver.$(Admin.confirmClassCreate)
+    await confirmButton.click()
+
+},
+verifyNewEmptyClassCreated: async function (className = '11c', numOfStudents = '0') {
+    const allClassesContainer = await driver.$('tbody[data-testid=\'students_names_container\']')
+    const allClassesContent = await allClassesContainer.getText()
+    const contentArray = allClassesContent.split(" ")
+
+    const currentYear = new Date().getFullYear().toString().substring(2) // 20
+
+    expect(contentArray.length).to.equal(3) // teacher column should be empty and therefore not 4, but 3
+    expect(contentArray[0]).to.equal('11c')
+    expect(contentArray[1].includes(currentYear)).to.equal(true)
+    expect(contentArray[2]).to.equal(numOfStudents)
+
+},
 createNewPupil: async function(firstname, lastname, email) {
     name=firstname;
     eMAIL = email;
---
+++ step_definitions/adminCreateClassWithoutParticipants-steps.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const adminLogin = require('../page-objects/adminLogin')
+const administrationHelper = require("../page-objects/administration")
+const loginData = require('../shared-objects/loginData');
+
+const firstLogin = require('../shared_steps/firstLogin.js');
+
+
+Given(/^admin arrives on the Schul-Cloud page$/, function () {
+    return helpers.loadPage(loginData.url, 10);
+});
+Given(/^admin is logged in successfully$/, async function () {
+    const { defaultAdminUsername, defaultAdminPassword } = loginData
+    return adminLogin.performLogin(defaultAdminUsername, defaultAdminPassword)
+});
+Given(/^admin performs the first login steps$/, function () {
+    return firstLogin.firstLoginAdmin();
+});
+
+When(/^admin creates a class (.*)$/, function (className) {
+    return administrationHelper.createNewClass(className)
+});
+
+Then(/^admin should see the class (.*) with (.*) participants.$/, function (className, participants) {
+    return administrationHelper.verifyNewEmptyClassCreated(className, participants)
+});
