22faeb212904167a66caba0de45fbd9061229575
Jan Renz <jan.renz@hpi.de>
2020-08-04 19:12:06 +0200

    Update fetch.github.sh

--- scripts/ci/fetch.github.sh
+++ scripts/ci/fetch.github.sh
@@ -36,17 +36,17 @@ echo "BRANCH: $BRANCH_NAME"
 
 # fetch default (develop) script
 echo "try fetching script from default branch"
-curl -fO "https://raw.githubusercontent.com/schul-cloud/integration-tests/develop/scripts/ci/integration-test.github.sh" || true
+curl -fO "https://raw.githubusercontent.com/hpi-schul-cloud/integration-tests/develop/scripts/ci/integration-test.github.sh" || true
 
 # use master as default for releases & hotfixes
 if [[ $BRANCH_NAME = release* || $BRANCH_NAME = hotfix* ]];
 then
 	echo "try fetching script from master branch"
-	curl -fO "https://raw.githubusercontent.com/schul-cloud/integration-tests/master/scripts/ci/integration-test.github.sh" || true
+	curl -fO "https://raw.githubusercontent.com/hpi-schul-cloud/integration-tests/master/scripts/ci/integration-test.github.sh" || true
 fi
 # use branch specific script if available
 echo "try fetching script from $BRANCH_NAME branch"
-curl -fO "https://raw.githubusercontent.com/schul-cloud/integration-tests/$BRANCH_NAME/scripts/ci/integration-test.github.sh" || true
+curl -fO "https://raw.githubusercontent.com/hpi-schul-cloud/integration-tests/$BRANCH_NAME/scripts/ci/integration-test.github.sh" || true
 
 ls -a
 chmod 700 integration-test.github.sh
