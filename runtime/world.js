fc60cf70b4b98a1253d1ea2a0c8e41af8ff376cb
Jan Renz <jan.renz@gmail.com>
2020-08-04 22:11:41 +0200

    revert experiments

--- runtime/world.js
+++ runtime/world.js
@@ -340,7 +340,6 @@ AfterAll(function(done) {
  */
 After(async function(scenario) {
 	let driver = global.driver;
-	driver.pause(DELAY_3_SECOND);
 	if (scenario.result.status === Status.FAILED) {
 		if (remoteService && remoteService.type === 'browserstack') {
 			await driver.deleteSession();
@@ -356,7 +355,6 @@ After(async function(scenario) {
 			await driver.deleteSession();
 		}	
 	}
-	driver.pause(DELAY_3_SECOND);
 });
 
 /**
