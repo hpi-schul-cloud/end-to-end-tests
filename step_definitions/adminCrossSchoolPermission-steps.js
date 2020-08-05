839c1ed707dd38731206676375b7f22b80acb77e
kgorner <65538410+kgorner@users.noreply.github.com>
2020-08-05 10:49:05 +0200

    paths to not existing file deleted, steps replaced with common ones

--- step_definitions/adminCrossSchoolPermission-steps.js
+++ step_definitions/adminCrossSchoolPermission-steps.js
@@ -6,17 +6,6 @@ const firstLogin = require('../shared_steps/firstLogin.js');
 const elementHelpers = require('../runtime/helpers/elementHelpers.js');
 const administration = require("../page-objects/administration.js")
 
-
-
-
-
-Given(/^an admin is logged in$/, async function () {
-    await elementHelpers.loadPage(loginData.url, 10);
-    await startPage.clickLoginBtn();
-    await loginPage.performLogin('admin@schul-cloud.org', 'Schulcloud1!');
-    await firstLogin.firstLoginAdmin();
-});
-
 Given(/^an admin are able to request information about students from own school$/, async function () {
     await administration.getStudentsFromSameSchoolAndVerify()
 });
