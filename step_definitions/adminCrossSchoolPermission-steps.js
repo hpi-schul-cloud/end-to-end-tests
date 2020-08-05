e239ebf945913134c7a15b45236871ac0b6b0037
kgorner <65538410+kgorner@users.noreply.github.com>
2020-08-05 11:28:38 +0200

    filename  to common steps changed -> adapted

--- step_definitions/adminCrossSchoolPermission-steps.js
+++ step_definitions/adminCrossSchoolPermission-steps.js
@@ -3,6 +3,7 @@ const loginData = require('../shared-objects/loginData');
 const firstLogin = require('../shared_steps/firstLogin.js');
 const elementHelpers = require('../runtime/helpers/elementHelpers.js');
 const administration = require("../page-objects/administration.js")
+const common = require('../shared_steps/common-steps.js');
 
 Given(/^an admin are able to request information about students from own school$/, async function () {
     await administration.getStudentsFromSameSchoolAndVerify()
