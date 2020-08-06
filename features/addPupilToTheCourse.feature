95713c16e38fe991a713a9abab16d3a122a2c5d5
a.stan <alicja.stankiewicz@capgemini.com>
2020-08-06 15:10:51 +0200

    Fixed merged artifacts

--- features/addPupilToTheCourse.feature
+++ features/addPupilToTheCourse.feature
@@ -1,6 +1,5 @@
 @addStudent
 
-
 Feature: logging in as a teacher and create a new course and add pupils and classes to the course
 I want to be able to add some pupils to a new course on Schul-Cloud
 
@@ -12,8 +11,10 @@ Background:
 
 Scenario Outline:
 	When teacher creates a course <courseName> and adds student <studentName> to this course
+
 	Then teacher sees that participants icon in course with name <courseName> has correct number of members <studentName>
 	Then teacher clicks the participants icon in the course <courseName> and sees the added student <studentName> there
+
 Examples:
 |courseName|studentName|
 |Mathe     |Paula Meyer|
--- step_definitions/addPupilsToTheCourse-steps.js
+++ step_definitions/addPupilsToTheCourse-steps.js
@@ -1,21 +1,15 @@
 const addCoursePage = require("../page-objects/pages/coursePages/CRSSAddCoursePage");
 const courseListPage = require("../page-objects/pages/coursePages/CRSSCourseListPage");
 
-When(
-	/^.*creates a course (.*) and adds student (.*)to this course$/, async function (courseName, studentName) {
-		await addCoursePage.createCourseWithStudents(courseName, studentName);
-	}
-);
+When(/^.*creates a course (.*) and adds student (.*)to this course$/, async function (courseName, studentName) {
+    await addCoursePage.createCourseWithStudents(courseName, studentName);
+});
 
-Then(
-	/^.*sees that participants icon in course with name (.*) has correct number of members (.*)$/, async function (courseName, studentName) {
-		await courseListPage.isCorrectNumberOfMembersInCourseForSection(courseName, [studentName], courseListPage.section.activeCourses);
-	
-	}
-);
+Then(/^.*sees that participants icon in course with name (.*) has correct number of members (.*)$/, async function (courseName, studentName) {
+    await courseListPage.isCorrectNumberOfMembersInCourseForSection(courseName, [studentName], courseListPage.section.activeCourses);
 
-Then(
-	/^.*clicks the participants icon in the course (.*) and sees the added student (.*) there$/, async function (courseName, studentName) {
-		await courseListPage.areMembersOnTheListInCourseForSection(courseName, [studentName], courseListPage.section.activeCourses);
-	}
-);
+});
+
+Then(/^.*clicks the participants icon in the course (.*) and sees the added student (.*) there$/, async function (courseName, studentName) {
+    await courseListPage.areMembersOnTheListInCourseForSection(courseName, [studentName], courseListPage.section.activeCourses);
+});
