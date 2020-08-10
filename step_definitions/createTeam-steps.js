5a2601606b3a88bfd7555521ed38054cff8954a6
yuliashikh <yulia.shikhareva@gmail.com>
2020-08-10 14:42:00 +0200

    function call new loation fix

--- step_definitions/createTeam-steps.js
+++ step_definitions/createTeam-steps.js
@@ -4,7 +4,7 @@ const loginPage = require('../page-objects/pages/generalPagesBeforeLogin/LoginPa
 const startPage = require('../page-objects/pages/generalPagesBeforeLogin/StartPageBeforeLogin.js');
 const loginData = require('../shared-objects/loginData');
 const createTeam = require('../page-objects/createTeam');
-const newPupil = require('../page-objects/administration');
+const createStudent = require('../page-objects/pages/administrationPages/ADMNSTRTNAdministerStudentsPage');
 const Login = require('../shared-objects/loginData');
 const elementHelpers = require('../runtime/helpers/elementHelpers.js');
 let fullname1;
@@ -16,27 +16,30 @@ let teamName;
 Given(/^the teacher started on the login page and$/, function() {
     return elementHelpers.loadPage(loginData.url, 10);
   });
+
 Given(/^teacher successfully logged in$/, async function() {
 
     await startPage.clickLoginBtn();
     await loginPage.performLogin(Login.defaultTeacherUsername,Login.defaultTeacherpassword);
   });
+
   When(/^teacher adds a new student with (.*), (.*), (.*)$/,  function(firstname1, lastname1, email1) {
     fullname1 = firstname1+" "+lastname1;
-    return newPupil.createNewPupil(firstname1, lastname1, email1);
+    return createStudent.createNewPupil(firstname1, lastname1, email1)
   });
+
   When(/^teacher adds one more student with (.*), (.*), (.*)$/,  function(firstname2, lastname2, email2) {
     fullname2 = firstname2+" "+lastname2;
-    return newPupil.createNewPupil(firstname2, lastname2, email2);
+    return createStudent.createNewPupil(firstname2, lastname2, email2);
   });
 
   When(/^teacher creates a new team with (.*) and$/,  function(teamname) {
     teamName = teamname;
     return createTeam.createTeamAndGoToSettings(teamname);
   });
-When(/^teacher adds two students to this team$/, function() {
-  return createTeam.addTwoTeamMemebers(fullname1, fullname2)
 
+When(/^teacher adds two students to this team$/, function() {
+  return createTeam.addTwoTeamMemebers(fullname1, fullname2);
   });
 
 Then(/^this team should be displayed on the team page$/, async function() {
