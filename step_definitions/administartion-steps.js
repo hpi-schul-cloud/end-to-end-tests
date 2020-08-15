02b848651b3a04040ac1b0044c84e157ba3ff772
yuliashikh <yulia.shikhareva@gmail.com>
2020-08-15 16:50:35 +0200

    .

--- step_definitions/administartion-steps.js
+++ step_definitions/administartion-steps.js
@@ -5,7 +5,7 @@ const ADMNSTRTNAdministrationOverviewPage = require('../page-objects/pages/admin
 const loginPage = require('../page-objects/pages/generalPagesBeforeLogin/LoginPage.js');
 const startPage = require('../page-objects/pages/generalPagesBeforeLogin/StartPageBeforeLogin.js');
 const loginData = require('../shared-objects/loginData');
-const navigationTopPage = require('../page-objects/pages/NavigationTopPage');
+
 
 
 Given(/^this admin logs in successfully$/, async function () {
