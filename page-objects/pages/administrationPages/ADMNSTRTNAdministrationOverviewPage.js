a2546911997de2de2a2fa03c8368e6107224f11e
yuliashikh <yulia.shikhareva@gmail.com>
2020-08-20 12:50:12 +0200

    fix

--- page-objects/pages/administrationPages/ADMNSTRTNAdministrationOverviewPage.js
+++ page-objects/pages/administrationPages/ADMNSTRTNAdministrationOverviewPage.js
@@ -4,24 +4,27 @@ const elementHelpers = require('../../../runtime/helpers/elementHelpers');
 const waitHelpers = require('../../../runtime/helpers/waitHelpers');
 const urlAdministration = `${CLIENT.URL}/administration`;
 
-module.exports = {
-  
+const selectors = {
     administrateStudentsBtn: 'div[data-testid=\'administrate_students\']',
     administrateTeachersBtn: 'div[data-testid=\'administrate_teachers\']',
     administrateClassesBtn:'div[data-testid=\'administrate_classes\']',
     administrationsTabs: 'ul.subitems span',
+};
+
+module.exports = {
+    selectors,
 
     goToAdministrationPage: async function() {
         await elementHelpers.loadPage(urlAdministration, 10)
     },
     clickAdministrateStudents: async function() {
-        await waitHelpers.waitAndClick(administrateStudentsBtn);
+        await waitHelpers.waitAndClick(selectors.administrateStudentsBtn);
     },
     clickAdministrateClasses: async function() {
-        await waitHelpers.waitAndClick(administrateClassesBtn);
+        await waitHelpers.waitAndClick(selectors.administrateClassesBtn);
     },
     clickAdministrateTeachers: async function() {
-        await waitHelpers.waitAndClick(administrateTeachersBtn);
+        await waitHelpers.waitAndClick(selectors.administrateTeachersBtn);
     },
 }
 
