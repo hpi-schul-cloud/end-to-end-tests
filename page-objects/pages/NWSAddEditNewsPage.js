a84b6d68f8cc42e33faed0c108e3e0eeb44c9565
yuliashikh <yulia.shikhareva@gmail.com>
2020-08-21 16:14:25 +0200

    news fix

--- page-objects/pages/NWSAddEditNewsPage.js
+++ page-objects/pages/NWSAddEditNewsPage.js
@@ -8,10 +8,7 @@ const waitHelpers = require('../../runtime/helpers/waitHelpers');
 const selectors = {
     timeNewsMustBePublished: 'input[data-testid="news_date_to_be_displayed"]',
     submitNewsBtn: 'button[data-testid="btn_news_submit"]',
-    titleField: 'input.h1',
-    contentField: '.editor [contenteditable="true"]',
-    dateSelector: '[data-testid="news_date"] input',
-   
+    newsTitle: '[data-testid="news_title"]',
 };
 
 module.exports = {
@@ -21,10 +18,12 @@ goToNewNews: async function() {
     await elementHelpers.loadPage(url, 100);
 },
 setTitle: async function(title){
-    await waitHelpers.waitAndSetValue(selectors.titleField, title)
+    await waitHelpers.waitAndSetValue(selectors.newsTitle, title)
 },
 setContent: async function(content){
-    await waitHelpers.waitAndSetValue(selectors.contentField, content);
+    let contentField = await driver.$('.editor [contenteditable="true"]');
+    await contentField.waitForExist(2500);
+    await contentField.setValue(content);
 },
 setPublishDate: async function(date) {
     await waitHelpers.waitAndSetValue(selectors.dateSelector,date)
