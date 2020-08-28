91658363368a9c3d81a0cb7a980a8abffab8a4b2
yuliashikh <yulia.shikhareva@gmail.com>
2020-08-28 09:42:48 +0200

    try new default pswd  export

--- step_definitions/administartion-steps.js
+++ step_definitions/administartion-steps.js
@@ -2,6 +2,8 @@
 
 const ADMNSTRTNAdministrationOverviewPage = require('../page-objects/pages/administrationPages/ADMNSTRTNAdministrationOverviewPage');
 const studentAdministration = require('../page-objects/pages/administrationPages/ADMNSTRTNAdministerStudentsPage');
+const startPage = require('../page-objects/pages/generalPagesBeforeLogin/StartPageBeforeLogin');
+const loginPage = require('../page-objects/pages/generalPagesBeforeLogin/LoginPage');
 
 
 
@@ -19,7 +21,9 @@ Then(/^.* manually submits a consent (.*)$/, function (email) {
 	return studentAdministration.submitConsent(email);
 });
 Then(/^new pupil (.*) can log in with default password$/, async function (email) {
-	await studentAdministration.studentLogsInWithDefaultPassword(email)
+	//await studentAdministration.studentLogsInWithDefaultPassword(email);
+	await startPage.clickLoginBtn();
+	await loginPage.performLogin(email, studentAdministration.oldPassword)
 });
 
 
