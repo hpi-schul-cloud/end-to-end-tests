a929a6583d1ebfa316ae7bb25a3a76de71ebd3db
a.stan <alicja.stankiewicz@capgemini.com>
2020-09-24 01:16:44 +0200

    deepcode fixes

--- page-objects/pages/coursePages/CRSSCourseHomeworksPage.js
+++ page-objects/pages/coursePages/CRSSCourseHomeworksPage.js
@@ -1,19 +1,19 @@
 /*[url/courses]/[courseId]/[activeTab=homeworks]]*/
-"use strict";
-const waitHelpers = require('../../../runtime/helpers/waitHelpers.js');
-const courseListPage = require("../../../page-objects/pages/coursePages/CRSSCourseListPage");
-const coursePage = require("../../../page-objects/pages/coursePages/CRSSGeneralCoursePage");
+"use strict"
+const waitHelpers = require("../../../runtime/helpers/waitHelpers.js")
+const courseListPage = require("../../../page-objects/pages/coursePages/CRSSCourseListPage")
+const coursePage = require("../../../page-objects/pages/coursePages/CRSSGeneralCoursePage")
 
-const addNewHomeworkBtn = "[data-section='js-homeworks'] .add-button a";
+const addNewHomeworkBtn = "[data-section='js-homeworks'] .add-button a"
 
 module.exports = {
-	clickAddNewTaskBtn: async function () {
-		await waitHelpers.waitAndClick(addNewHomeworkBtn);
-	},
+    clickAddNewTaskBtn: async function () {
+        await waitHelpers.waitAndClick(addNewHomeworkBtn)
+    },
 
-	clickAddNewTaskInCourse: async function (coursename) {
-		await courseListPage.clickOnCourseInSection(coursename, courseListPage.section.activeCourses);
-		await coursePage.openHomeworksTab();
-		await this.clickAddNewTaskBtn();
-	}
-};
+    clickAddNewTaskInCourse: async function (coursename) {
+        await courseListPage.clickOnCourseInSection(coursename, courseListPage.section.activeCourses)
+        await coursePage.openHomeworksTab()
+        await this.clickAddNewTaskBtn()
+    },
+}
