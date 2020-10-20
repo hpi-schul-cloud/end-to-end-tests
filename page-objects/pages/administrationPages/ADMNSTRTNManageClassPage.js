918523fada67b9ac040c407f62e7f70385838d21
d3n153 <65532127+d3n153@users.noreply.github.com>
2020-10-20 19:23:26 +0200

    Update ADMNSTRTNManageClassPage.js

--- page-objects/pages/administrationPages/ADMNSTRTNManageClassPage.js
+++ page-objects/pages/administrationPages/ADMNSTRTNManageClassPage.js
@@ -4,7 +4,6 @@ const elementHelpers = require("../../../runtime/helpers/elementHelpers")
 
 const editClassBtn = "a.d-block.mb-1"
 
-
 async function clickEditClassBtn() {
     await elementHelpers.clickAndWait(editClassBtn)
 }
