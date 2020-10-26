c3aeddeb4c64ad7a1e0d8c96fb583f4a50ef6a18
Janns <paul.janns@capgemini.com>
2020-10-26 11:40:38 +0100

    changed to not using env because failing

--- runtime/helpers/mailCatcher.js
+++ runtime/helpers/mailCatcher.js
@@ -1,6 +1,6 @@
 'use strict';
 const axios = require('axios');
-let mailCatcherAPI = process.env.MAILCATCH_API;
+let mailCatcherAPI = 'localhost:1080/messages';
 
 async function receiveEmails() {
 	let res = await axios.get(mailCatcherAPI);
