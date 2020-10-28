53e80331b4b604b48c45c8e18cfa6b5a4c9d5ca2
Meyer <denise.meyer@capgemini.com>
2020-10-28 18:05:09 +0100

    added lines

--- step_definitions/administration-steps.js
+++ step_definitions/administration-steps.js
@@ -7,16 +7,18 @@ When(/^admin goes to students administration$/, function () {
 	return ADMNSTRTNAdministrationOverviewPage.clickAdministrateStudents();
 });
 
-When(/^.*set student firstname '([^']*)', lastname '([^']*)', email '([^']*)'$/, function (firstname,secondname,email) {
-	return studentAdministration.createNewPupil(firstname, secondname, email);
+When(/^.*set student firstname '([^']*)', lastname '([^']*)', email '([^']*)'$/, function (firstname, lastname, email) {
+	return studentAdministration.createNewPupil(firstname, lastname, email);
 });
 
 Then(/^the admin should see new pupil with email '([^']*)' among his pupils$/, function (email) {
 	return studentAdministration.isStudentEmailOnTheList(email);
 });
+
 Then(/^.* manually submits a consent '([^']*)'$/, function (email) {
 	return studentAdministration.submitConsent(email);
 });
+
 Then(/^new pupil '([^']*)' can log in with default password$/, async function (email) {
 	await studentAdministration.studentLogsInWithDefaultPassword(email);
 });
