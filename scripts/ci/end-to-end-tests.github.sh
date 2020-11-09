52e2ae4968264b9b1dc5f2f74a2f7af2d4430a9a
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2020-11-09 16:15:55 +0100

    no mongoimport in docker container

--- scripts/ci/end-to-end-tests.github.sh
+++ scripts/ci/end-to-end-tests.github.sh
@@ -69,15 +69,13 @@ install(){
 	cd ..
 
 	echo "INSTALL DEPENDNECIES..."
-	#cd schulcloud-server && npm ci && cd ..
+	cd schulcloud-server && npm ci && cd ..
 	cd end-to-end-tests && npm ci && cd ..
 	echo "INSTALL DEPENDNECIES DONE"
 }
 
 before(){
-	./startup_end-to-end-tests.sh exec server npm run setup
-	./startup_end-to-end-tests.sh exec server npm run seed
-	#cd schulcloud-server && npm run setup && npm run seed && cd ..
+	cd schulcloud-server && npm run setup && npm run seed && cd ..
 
 	# wait for the nuxt client to be available
 	echo "waiting max 150s for nuxt to be available"
