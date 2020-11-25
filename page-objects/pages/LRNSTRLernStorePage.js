d974af1999b9482dd570286c31b32efb3a610638
yuliashikhareva <yulia.shikhareva@gmail.com>
2020-11-25 21:58:14 +0100

    fixed selector

--- page-objects/pages/LRNSTRLernStorePage.js
+++ page-objects/pages/LRNSTRLernStorePage.js
@@ -13,7 +13,7 @@ let title;
 const selectorSearchField = ".input-active.search__container > input";
 const selectorNumberOfContentOnGUI = ".content__total";
 const selectorFirstElement = ".grid > section:nth-child(1) .content";
-const selectorTitleOfMaterialWhenClicked = ".content .title > span";
+const selectorTitleOfMaterialWhenClicked = ".content-container > .title > span";
 const selectorMaterialContainer = "[data-testid='lernStoreCardsContainer']"
 const selectorCourseTitles = "div.multiselect__content-wrapper > ul > li > span";
 const selectorSubmitAddToCourseAndTopic = '[data-testid="modal_submit_btn"]';
