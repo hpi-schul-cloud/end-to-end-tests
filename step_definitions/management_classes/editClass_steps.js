51f988d3cf0c06aec0902f71c5aaae93af76650d
Grzesiek80 <38048152+Grzesiek80@users.noreply.github.com>
2020-12-07 08:46:21 +0100

    create new test editClass

--- step_definitions/management_classes/editClass_steps.js
+++ step_definitions/management_classes/editClass_steps.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const manageClass = require('../../page-objects/pages/managementPages/ManageClassPage');
+const manageClassesPage = require('../../page-objects/pages/managementPages/ManageClassesPage');
 
 When(/^.* edits custom class name to '([^']*)' and class school year to '([^']*)'$/, async function (customClassName, schoolYear) {
 	await manageClass.editClass({schoolYear: schoolYear, customClassName: customClassName});
@@ -8,4 +9,28 @@ When(/^.* edits custom class name to '([^']*)' and class school year to '([^']*)
 
 Then(/^.* class with name '([^']*)' and teacher lastname '([^']*)' is visible$/, function (newClassName, teacherLastname) {
     return manageClass.isClassEdited(newClassName, teacherLastname, true)
+});
+
+When(/^.* chooses class with name '([^']*)' and click managemnt class$/, function (className) {
+	return manageClassesPage.clickManagmentClassByNameBtn(className);
+});
+
+When(/^.*adds a student to class with name '([^']*)'$/, async function (fullName) {
+	await manageClass.setStudent(fullName);
+});
+
+When(/^.*adds a teacher to class with name '([^']*)'$/, async function (fullName) {
+	await manageClass.setTeacher(fullName);
+});
+
+When(/^.*clicks on Save-changes in class button$/, async function () {
+	await manageClass.clickEditClassSaveChangesBtn();
+});
+
+When(/^.* delete student '([^']*)' from class$/, async function (studentName) {
+	await manageClass.deleteStudentFromClass(studentName);
+});
+
+When(/^.* delete teacher '([^']*)' from class$/, async function (teacherName) {
+	await manageClass.deleteTeacherFromClass(teacherName);
 });
