ed62c6bee23d00aebf9dcf3140963aef9f759481
D3ni53 <denise.meyer@capgemini.com>
2021-01-11 11:38:46 +0100

    first fix

--- page-objects/pages/managementPages/ManageClassesPage.js
+++ page-objects/pages/managementPages/ManageClassesPage.js
@@ -1,30 +1,30 @@
 /*[url/administration/classes]*/
 
-const elementHelpers = require("../../../runtime/helpers/elementHelpers")
+const elementHelpers = require('../../../runtime/helpers/elementHelpers');
 const waitHelpers = require('../../../runtime/helpers/waitHelpers');
 const tableHelpers = require('../../../runtime/helpers/tableHelpers');
 
-const addClassBtn = "a[data-testid='createClass']"
-const classesTabs = "a.tab span"
-const editClassBtn = "a[title='administration.classes.placeholer.change']"
+const addClassBtn = "a[data-testid='createClass']";
+const classesTabs = 'a.tab span';
+const editClassBtn = "a[title='administration.classes.placeholer.change']";
 const tableOfClasses = 'tbody[data-testid="students_names_container"] > tr';
 const editElements = '.table-actions i.fa-users';
 const classTable = 'table';
 
 async function clickCreateClassBtn() {
-    await elementHelpers.clickAndWait(addClassBtn)
+	await elementHelpers.clickAndWait(addClassBtn);
 }
 
 async function clickEditClassBtn() {
-    await elementHelpers.clickAndWait(editClassBtn);
+	await elementHelpers.clickAndWait(editClassBtn);
 }
 
- async function clickOnClassesTab(tabText) {
-    const element = await elementHelpers.getElementByText(classesTabs, tabText);
-     await elementHelpers.clickAndWait(element);
- }
+async function clickOnClassesTab(tabText) {
+	const element = await elementHelpers.getElementByText(classesTabs, tabText);
+	await elementHelpers.clickAndWait(element);
+}
 
- async function clickManagmentClassByNameBtn(className) {
+async function clickManagmentClassByNameBtn(className) {
 	await waitHelpers.waitUntilElementIsVisible(tableOfClasses);
 	let editsElements = await elementHelpers.getListOfAllElements(editElements);
 	let indexNameRow = await tableHelpers.getIndexOfRowContainsText(classTable, className);
@@ -34,22 +34,22 @@ async function clickEditClassBtn() {
 async function isNumberOfMembersInClass(className, numberOfMembers) {
 	await waitHelpers.waitUntilElementIsVisible(tableOfClasses);
 	let indexRow = await tableHelpers.getIndexOfRowContainsText(classTable, className);
-	let indexColumn = await tableHelpers.getIndexOfHeaderContainsText(classTable, 'Schüler');
-	let numberOfStudents = await elementHelpers.getElementText('tr:nth-child('+ (indexRow+1) +') > td:nth-child('+ (indexColumn+1) +')');
-    expect(numberOfMembers).to.equal(numberOfStudents)
+	let indexColumn = await tableHelpers.getIndexOfHeaderContainsText(classTable, 'Schüler:innen');
+	let numberOfStudents = await elementHelpers.getElementText(
+		'tr:nth-child(' + (indexRow + 1) + ') > td:nth-child(' + (indexColumn + 1) + ')'
+	);
+	expect(numberOfMembers).to.equal(numberOfStudents);
 }
-    // choose between className, teachers, students, schoolYear
- async function getClassDetailsList(cell) {
+// choose between className, teachers, students, schoolYear
+async function getClassDetailsList(cell) {
 	let names = await elementHelpers.getTextFromAllElements(cell);
 	return names;
 }
 
-
-
 module.exports = {
-    clickCreateClassBtn,
-    clickEditClassBtn,
-    clickOnClassesTab,
-    clickManagmentClassByNameBtn,
-    isNumberOfMembersInClass
-}
+	clickCreateClassBtn,
+	clickEditClassBtn,
+	clickOnClassesTab,
+	clickManagmentClassByNameBtn,
+	isNumberOfMembersInClass,
+};
