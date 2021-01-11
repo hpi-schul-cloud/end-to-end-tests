8f944271727f0b36433c35814c1fb51e8bcb89dc
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-01-11 14:45:20 +0100

    SC-8321 await left navigation items with children are loaded

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,46 +6,50 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 Allowed Types of change: `Added`, `Changed`, `Deprecated`, `Removed`, `Fixed`, `Security`
 
+## 25.4.3
+
+-   SC-8321 await left navigation items with children are loaded
+
 ## 25.3.0
 
 ### Added
 
-- SC-6814 - Delete class test
-- SC-5795 - CreateUser and send registration link
-- SC-7736 - Deleted User forgot password functionality
-- SC-7735 - Deleted User Tests for Removed user in not visible in team anymore
-- SC-7059 - DeletionConcept Tests
-- OPS-1297 - Added Changelog github action
-- SC-7571 - sendConsentFormEmail adjusted
-- SC-7733 - User can not login anymore, DeletionConcept Tests
-- SC-5797 - Added new test: showRegularTaskOnDashboard
-- SC-5780 - Added new test: editTeam
-- SC-6816 - Added new test: editClass
+-   SC-6814 - Delete class test
+-   SC-5795 - CreateUser and send registration link
+-   SC-7736 - Deleted User forgot password functionality
+-   SC-7735 - Deleted User Tests for Removed user in not visible in team anymore
+-   SC-7059 - DeletionConcept Tests
+-   OPS-1297 - Added Changelog github action
+-   SC-7571 - sendConsentFormEmail adjusted
+-   SC-7733 - User can not login anymore, DeletionConcept Tests
+-   SC-5797 - Added new test: showRegularTaskOnDashboard
+-   SC-5780 - Added new test: editTeam
+-   SC-6816 - Added new test: editClass
 
 ## Changed
 
-- IMP-160 rename repository
-- SC-7110 Refactoring: Improved terms in cucumber steps: Teams, Topics, Class
-- SC-7524-parametrization: adjust parametrization to existing feature files
-- SC-7684-parametrization: feature and step definition file restructuring
-- SC-7329-Adjust QA TCs to initial DB
-- SC-6767-Chrome driver update 2.46 -> 87.0
+-   IMP-160 rename repository
+-   SC-7110 Refactoring: Improved terms in cucumber steps: Teams, Topics, Class
+-   SC-7524-parametrization: adjust parametrization to existing feature files
+-   SC-7684-parametrization: feature and step definition file restructuring
+-   SC-7329-Adjust QA TCs to initial DB
+-   SC-6767-Chrome driver update 2.46 -> 87.0
 
 ### Fixed
 
-- fixed github action changelog
+-   fixed github action changelog
 
 ## 25.0.1 - 2020-09-30
 
 ### Changed
 
-- SC-6945 skip @copyCourseWithText for hotfix
-- SC-5858 change selectors after removing chosen library in the old client
-- SC-7878 - dropdowns and multiselects
+-   SC-6945 skip @copyCourseWithText for hotfix
+-   SC-5858 change selectors after removing chosen library in the old client
+-   SC-7878 - dropdowns and multiselects
 
 ## 25.0.0
 
-- OPS-1205 Handle German and English error message within login page test
+-   OPS-1205 Handle German and English error message within login page test
 
 ## [24.3.0] - 2020-08-25
 
@@ -54,29 +58,29 @@ Allowed Types of change: `Added`, `Changed`, `Deprecated`, `Removed`, `Fixed`, `
 ## [23.6.0] - 2020-07-21
 
 ### Fixed - 23.6.0
-### Fixed - 24.0.0
-- fixed the @createSimpleHomework, added one more step when creating a new course
 
+### Fixed - 24.0.0
+
+-   fixed the @createSimpleHomework, added one more step when creating a new course
 
 ## [23.5.0] - 2020-06-21
 
 ### Added in 23.5.0
 
-- Fix dependencies security vulnerabilities [SC-4534](https://github.com/hpi-schul-cloud/end-to-end-tests/pull/78)
-- Add timestamps to filenames [NA](https://github.com/hpi-schul-cloud/end-to-end-tests/pull/75)
-- Fix for failing test in @createSimpleHomework [NA](https://github.com/hpi-schul-cloud/end-to-end-tests/pull/72)
+-   Fix dependencies security vulnerabilities [SC-4534](https://github.com/hpi-schul-cloud/end-to-end-tests/pull/78)
+-   Add timestamps to filenames [NA](https://github.com/hpi-schul-cloud/end-to-end-tests/pull/75)
+-   Fix for failing test in @createSimpleHomework [NA](https://github.com/hpi-schul-cloud/end-to-end-tests/pull/72)
 
 ## [23.2.0] - 2020-06-29
 
 ### Added in 23.2.0
 
-- Admin can create empty classes
+-   Admin can create empty classes
 
 ## [23.0.0] - 2020-05-19
 
 ### Added in 23.0.0
 
-- copied in a basic mergify config to automerge PRs
-- added `npm ci` command to end-to-end-tests ci setup to be able to execute new js seed script properly
-- This changelog has been added
-
+-   copied in a basic mergify config to automerge PRs
+-   added `npm ci` command to end-to-end-tests ci setup to be able to execute new js seed script properly
+-   This changelog has been added
--- page-objects/pages/NavigationLeftPage.js
+++ page-objects/pages/NavigationLeftPage.js
@@ -73,148 +73,159 @@ const navItemHelpContact = navItemStringDefault2ndLevel.replace('XXX', 'Kontakt'
 const item = '.sidebar-list .link-name';
 const subitem = '.subitem .link-name';
 
-async function clickNavItemLogo () {
+async function clickNavItemLogo() {
 	await elementHelpers.clickAndWait(navItemLogo);
 }
 
-async function clickNavItemDashboard () {
+async function clickNavItemDashboard() {
 	await elementHelpers.clickAndWait(navItemDashboard);
 }
 
-
-async function clickNavItemCourses () {
+async function clickNavItemCourses() {
 	await elementHelpers.clickAndWait(navItemCourses);
 }
 
-async function clickNavItemTasks () {
+async function clickNavItemTasks() {
 	await elementHelpers.clickAndWait(navItemTasks);
+	const link = await elementHelpers.getLink(navItemTasks);
+	await elementHelpers.loadPage(link);
 	await waitHelpers.waitUntilAjaxIsFinished();
 }
 
-async function clickNavItemTasksAsked () {
+async function clickNavItemTasksAsked() {
 	await elementHelpers.clickAndWait(navItemTasksAsked);
 	await waitHelpers.waitUntilAjaxIsFinished();
 }
-async function clickNavItemTasksPrivate () {
+async function clickNavItemTasksPrivate() {
 	await elementHelpers.clickAndWait(navItemTasksPrivate);
 	await waitHelpers.waitUntilAjaxIsFinished();
 }
-async function clickNavItemTasksArchive () {
+async function clickNavItemTasksArchive() {
 	await elementHelpers.clickAndWait(navItemTasksArchive);
 	await waitHelpers.waitUntilAjaxIsFinished();
 }
 
-async function clickNavItemTeams () {
+async function clickNavItemTeams() {
 	await elementHelpers.clickAndWait(navItemTeams);
 }
 
-async function clickNavItemFiles () {
+async function clickNavItemFiles() {
 	await elementHelpers.clickAndWait(navItemFiles);
+	const link = await elementHelpers.getLink(navItemFiles);
+	await elementHelpers.loadPage(link);
 }
 
-async function clickNavItemFilesMy () {
+async function clickNavItemFilesMy() {
 	await elementHelpers.clickAndWait(navItemFilesMy);
 }
 
-async function clickNavItemFilesCourses () {
+async function clickNavItemFilesCourses() {
 	await elementHelpers.clickAndWait(navItemFilesCourses);
 }
 
-async function clickNavItemFilesTeams () {
+async function clickNavItemFilesTeams() {
 	await elementHelpers.clickAndWait(navItemFilesTeams);
 }
 
-async function clickNavItemFilesShared () {
+async function clickNavItemFilesShared() {
 	await elementHelpers.clickAndWait(navItemFilesShared);
 }
 
-async function clickNavItemNews () {
+async function clickNavItemNews() {
 	await elementHelpers.clickAndWait(navItemNews);
 }
 
-async function clickNavItemCalendar () {
+async function clickNavItemCalendar() {
 	await elementHelpers.clickAndWait(navItemCalendar);
 }
 
-async function clickNavItemContent () {
+async function clickNavItemContent() {
 	await elementHelpers.clickAndWait(navItemLearnstore);
 }
 
-async function clickNavItemAddons () {
+async function clickNavItemAddons() {
 	await elementHelpers.clickAndWait(navItemAddons);
 }
 
-async function clickNavItemManagement () {
+async function clickNavItemManagement() {
 	await elementHelpers.clickAndWait(navItemManagement);
+	const link = await elementHelpers.getLink(navItemManagement);
+	await elementHelpers.loadPage(link);
 }
 
-async function clickNavItemManageStudents () {
+async function clickNavItemManageStudents() {
 	await elementHelpers.clickAndWait(navItemManagementStudents);
 }
 
-async function clickNavItemManageTeachers () {
+async function clickNavItemManageTeachers() {
 	await elementHelpers.clickAndWait(navItemManagementTeachers);
 }
 
-async function clickNavItemManageCourses () {
+async function clickNavItemManageCourses() {
 	await elementHelpers.clickAndWait(navItemManagementCourses);
 }
 
-async function clickNavItemManageClasses () {
+async function clickNavItemManageClasses() {
 	await elementHelpers.clickAndWait(navItemManagementClasses);
 }
 
-async function clickNavItemManageTeams () {
+async function clickNavItemManageTeams() {
 	await elementHelpers.clickAndWait(navItemManagementTeams);
 }
 
-async function clickNavItemManageSchool () {
+async function clickNavItemManageSchool() {
 	await elementHelpers.clickAndWait(navItemManagementSchool);
 }
 
-async function clickNavItemHelpDesk () {
+async function clickNavItemHelpDesk() {
 	await elementHelpers.clickAndWait(navItemHelpDesk);
 }
 
-async function clickNavItemHelpArea () {
+async function clickNavItemHelpArea() {
 	await elementHelpers.clickAndWait(navItemHelparea);
+	const link = await elementHelpers.getLink(navItemFiles);
+	await elementHelpers.loadPage(link);
 }
 
-async function clickNavItemHelpArticles () {
+async function clickNavItemHelpArticles() {
 	await elementHelpers.clickAndWait(navItemHelparticles);
 }
 
-async function clickNavItemHelpTrainings () {
+async function clickNavItemHelpTrainings() {
 	await elementHelpers.clickAndWait(navItemHelpTrainings);
 }
 
-async function clickNavItemHelpContact () {
+async function clickNavItemHelpContact() {
 	await elementHelpers.clickAndWait(navItemHelpContact);
 }
 
-async function getListOfAllItems () {
+async function getListOfAllItems() {
 	return elementHelpers.getTextFromAllElements(item);
 }
 
-async function getListOfSubItems () {
+async function getListOfSubItems() {
 	return elementHelpers.getTextFromAllElements(subitem);
 }
 
-async function areSubMenuItemsVisible (listOfExpectedSubItems) {
-	listOfExpectedSubItems = listOfExpectedSubItems.split(",").map(item => {return item.trim();});
+async function areSubMenuItemsVisible(listOfExpectedSubItems) {
+	listOfExpectedSubItems = listOfExpectedSubItems.split(',').map((item) => {
+		return item.trim();
+	});
 	const listOfSubItems = await getListOfSubItems();
 	const isSame =
 		listOfExpectedSubItems.length == listOfSubItems.length &&
 		listOfSubItems.every(function (element, index) {
-			return element  === listOfSubItems[index];
+			return element === listOfSubItems[index];
 		});
 	const msg = 'List of expected sub items does not match with the current one\n';
 	const resultMsg = 'Expected: ' + listOfExpectedSubItems + '\n  Actual: ' + listOfSubItems;
 	expect(isSame, msg + resultMsg).to.equal(true);
 }
 
-async function areMenuItemsVisible (listOfExpectedItems) {
-	listOfExpectedItems = listOfExpectedItems.split(",").map(item => {return item.trim();});
+async function areMenuItemsVisible(listOfExpectedItems) {
+	listOfExpectedItems = listOfExpectedItems.split(',').map((item) => {
+		return item.trim();
+	});
 	const listOfItems = await getListOfAllItems();
 	const isSame =
 		listOfExpectedItems.length == listOfItems.length &&
