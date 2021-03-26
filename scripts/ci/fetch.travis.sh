d633622e874032363bc4a485131f3ddb8db381e3
Paul Janns <paulja@janns.de>
2021-03-26 11:27:41 +0100

    more testing with branch names

--- scripts/ci/fetch.travis.sh
+++ scripts/ci/fetch.travis.sh
@@ -4,12 +4,13 @@
 set -e
 
 export BRANCH_NAME=${TRAVIS_PULL_REQUEST_BRANCH:=$TRAVIS_BRANCH}
+export PR_BRANCH_NAME="core-e2e-text-execution"
 
 echo "BRANCH: $BRANCH_NAME"
 
 # fetch default (develop) script
 echo "try fetching script from default branch"
-curl -fO "https://raw.githubusercontent.com/hpi-schul-cloud/end-to-end-tests/develop/scripts/ci/end-to-end-tests-core.travis.sh" || true
+curl -fO "https://raw.githubusercontent.com/hpi-schul-cloud/end-to-end-tests/$PR_BRANCH_NAME/scripts/ci/end-to-end-tests-core.travis.sh" || true
 
 # use master as default for releases & hotfixes
 if [[ $BRANCH_NAME = release* || $BRANCH_NAME = hotfix* ]];
