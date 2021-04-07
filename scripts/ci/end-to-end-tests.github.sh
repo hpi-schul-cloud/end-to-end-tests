4ac6066cf32401ef1099924c9003c7651ae81688
Paul Janns <paulja@janns.de>
2021-04-07 13:48:51 +0200

    test

--- scripts/ci/end-to-end-tests.github.sh
+++ scripts/ci/end-to-end-tests.github.sh
@@ -103,7 +103,12 @@ before(){
 
 main(){
 	cd end-to-end-tests
-	npm run test
+	if [[ $BRANCH_NAME = release* || $BRANCH_NAME = hotfix* ]]
+	then 
+		npm run test
+	else 
+		npm run test:core
+	fi
 	cd ..
 }
 
