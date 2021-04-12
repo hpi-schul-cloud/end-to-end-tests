ffa8f2684d86d200cf3bc2239ba0cb833d186b53
Paul Janns <paulja@janns.de>
2021-04-12 16:04:28 +0200

    fixed missing translation and removed createSimpleTopic

--- features/courses_topics/createTopic.feature
+++ features/courses_topics/createTopic.feature
@@ -4,20 +4,6 @@ Feature: Set of tests to copy courses
     Background: User opens Schul-cloud homepage Website
         Given user arrives on the Schul-Cloud homepage
 
-    @createSimpleTopic
-    Scenario Outline: As a user I want to be able to create a new topic in course
-        Given <userRole> logs in with email '<username>' and password '<password>'
-        And <userRole> goes to courses page
-        When <userRole> chooses course with name '<courseName>'
-        And <userRole> adds a topic with name '<topicName>'
-        And <userRole> clicks Save-changes button
-        Then <userRole> should see that topic with name '<topicName>' is visible on the list
-        When <userRole> clicks on topic with name '<topicName>'
-        Then <userRole> should see that topic title is '<topicName>'
-        Examples:
-            | userRole | username                        | password       | courseName | topicName |
-            | teacher  | karl.teacher.qa@schul-cloud.org | Schulcloud1qa! | Mathe      | Division  |
-
     @createTopicWithContent @e2eCore
     Scenario Outline: As a user, I want to be able to copy course with certain text
         Given <userRole> logs in
@@ -27,6 +13,8 @@ Feature: Set of tests to copy courses
         And <userRole> adds content <contentType> with title '<contentTitle>' and description '<contentDescription>'
         And <userRole> clicks Save-changes button
         Then <userRole> should see that copied course with name '<courseName>' contains topic with name '<topicName>'
+        When <userRole> clicks on topic with name '<topicName>'
+        Then <userRole> should see that topic title is '<topicName>'
         Examples:
             | userRole | courseName | topicName         | contentType | contentTitle          | contentDescription        |
             | teacher  | Mathe      | sample topic name | Text        | test text content     | test description          |
--- page-objects/pages/managementPages/ManageClassPage.js
+++ page-objects/pages/managementPages/ManageClassPage.js
@@ -1,79 +1,79 @@
 /*[url/administration/classes[classId]/edit]*/
 /*[url/administration/classes]/create*/
 
-const elementHelpers = require("../../../runtime/helpers/elementHelpers")
-const waitHelpers = require("../../../runtime/helpers/waitHelpers")
-const manageClassesPage = require("./ManageClassesPage")
+const elementHelpers = require('../../../runtime/helpers/elementHelpers');
+const waitHelpers = require('../../../runtime/helpers/waitHelpers');
+const manageClassesPage = require('./ManageClassesPage');
 
-const saveChangesBtn = "div.modal-footer.mt-1  button"
-const moreOptionsBtn = "a[data-testid='classCreationExtraOptions']"
-const classNameInput = "input[name='classsuffix']"
-const customClassNameInput = "input[name='classcustom']"
-const classGradeSelect = "select[name='grade'] + div.chosen-container"
-const schoolYearSelect = "select[name='schoolyear'] + div.chosen-container"
-const teachersMultiSelect = "select[name='teacherIds[]'] + div.chosen-container"
-const studentsMultiSelect = "select[name='userIds'] + div.chosen-container"
-const addClassConfirmBtn = "button[data-testId='confirmClassCreate']"
-const classListTable = "table [data-testid='students_names_container']"
-const deleteClassBtn = ".table-actions .btn-delete"
-const deleteButtonConfirmation = ".btn-submit"
-const deleteNameStudent = "select[name='userIds'] + div.chosen-container  span"
-const deleteStudentIcon = "select[name='userIds'] + div.chosen-container  a"
-const deleteTeacherName = "select[name='teacherIds[]'] + div.chosen-container  span"
-const deleteTeacherIcon = "select[name='teacherIds[]'] + div.chosen-container  a"
+const saveChangesBtn = 'div.modal-footer.mt-1  button';
+const moreOptionsBtn = "a[data-testid='classCreationExtraOptions']";
+const classNameInput = "input[name='classsuffix']";
+const customClassNameInput = "input[name='classcustom']";
+const classGradeSelect = "select[name='grade'] + div.chosen-container";
+const schoolYearSelect = "select[name='schoolyear'] + div.chosen-container";
+const teachersMultiSelect = "select[name='teacherIds[]'] + div.chosen-container";
+const studentsMultiSelect = "select[name='userIds'] + div.chosen-container";
+const addClassConfirmBtn = "button[data-testId='confirmClassCreate']";
+const classListTable = "table [data-testid='students_names_container']";
+const deleteClassBtn = '.table-actions .btn-delete';
+const deleteButtonConfirmation = '.btn-submit';
+const deleteNameStudent = "select[name='userIds'] + div.chosen-container  span";
+const deleteStudentIcon = "select[name='userIds'] + div.chosen-container  a";
+const deleteTeacherName = "select[name='teacherIds[]'] + div.chosen-container  span";
+const deleteTeacherIcon = "select[name='teacherIds[]'] + div.chosen-container  a";
 
 async function clickEditClassSaveChangesBtn() {
-    await elementHelpers.clickAndWait(saveChangesBtn)
+	await elementHelpers.clickAndWait(saveChangesBtn);
 }
 
 async function clickMoreOptionsBtn() {
-    await elementHelpers.clickAndWait(moreOptionsBtn)
+	await elementHelpers.clickAndWait(moreOptionsBtn);
 }
 
 async function clickAddClassConfirmation() {
-    await elementHelpers.clickAndWait(addClassConfirmBtn)
+	await elementHelpers.clickAndWait(addClassConfirmBtn);
 }
 
 async function setClassName(className) {
-    await waitHelpers.waitAndSetValue(classNameInput, className)
+	await waitHelpers.waitAndSetValue(classNameInput, className);
 }
 
 async function deleteStudentFromClass(studentName) {
-    await waitHelpers.waitUntilAjaxIsFinished();
-    let studentsNames = await elementHelpers.getListOfAllElements(deleteNameStudent);
-    let deleteStudentIcons = await elementHelpers.getListOfAllElements(deleteStudentIcon);
+	await waitHelpers.waitUntilAjaxIsFinished();
+	let studentsNames = await elementHelpers.getListOfAllElements(deleteNameStudent);
+	let deleteStudentIcons = await elementHelpers.getListOfAllElements(deleteStudentIcon);
 	for (let index = 0; index <= studentsNames.length; index++) {
 		let name = await elementHelpers.getElementText(studentsNames[index]);
 		if (studentName === name) {
 			await elementHelpers.clickAndWait(deleteStudentIcons[index]);
 			break;
 		}
-    }
+	}
 }
 
 async function deleteTeacherFromClass(teacherName) {
-    await waitHelpers.waitUntilAjaxIsFinished();
-    let teachersNames = await elementHelpers.getListOfAllElements(deleteTeacherName);
-    let deleteTeacherIcons = await elementHelpers.getListOfAllElements(deleteTeacherIcon);
+	await waitHelpers.waitUntilAjaxIsFinished();
+	let teachersNames = await elementHelpers.getListOfAllElements(deleteTeacherName);
+	let deleteTeacherIcons = await elementHelpers.getListOfAllElements(deleteTeacherIcon);
 	for (let index = 0; index <= teachersNames.length; index++) {
 		let name = await elementHelpers.getElementText(teachersNames[index]);
 		if (teacherName === name) {
 			await elementHelpers.clickAndWait(deleteTeacherIcons[index]);
 			break;
 		}
-    }
+	}
 }
 
 async function setCustomClassName(customClassName) {
-    await waitHelpers.waitAndSetValue(customClassNameInput, customClassName)
+	await waitHelpers.waitAndSetValue(customClassNameInput, customClassName);
 }
 
 async function setClassGrade(classGrade) {
-    await elementHelpers.selectOptionsByText(classGradeSelect, classGrade)
+	await elementHelpers.selectOptionsByText(classGradeSelect, classGrade);
 }
 
 async function setSchoolYear(schoolYear) {
-    await elementHelpers.selectOptionsByText(schoolYearSelect, schoolYear)
+	await elementHelpers.selectOptionsByText(schoolYearSelect, schoolYear);
 }
 
 async function setTeacher(name) {
@@ -89,84 +89,69 @@ async function setStudent(name) {
 function createListOfStudents(numberOfStudents, firstname, lastname) {
 	let listOfStudentNames = [];
 	for (let studentNumber = 1; studentNumber <= numberOfStudents; studentNumber++) {
-        listOfStudentNames.push(`${firstname}${studentNumber} ${lastname}${studentNumber}`)
+		listOfStudentNames.push(`${firstname}${studentNumber} ${lastname}${studentNumber}`);
 	}
 	return listOfStudentNames;
 }
 
-async function editClass({schoolYear, teachers, classGrade, className, customClassName}) {
-    await manageClassesPage.clickEditClassBtn()
-    if (schoolYear) await setSchoolYear(schoolYear);
-    if (teachers) await setTeacher(teachers);
-    if (classGrade) await setClassGrade(classGrade);
-    if (className) await setClassName(className);
-    if (customClassName) await setCustomClassName(customClassName);
-    await clickEditClassSaveChangesBtn()
+async function editClass({ schoolYear, teachers, classGrade, className, customClassName }) {
+	await manageClassesPage.clickEditClassBtn();
+	if (schoolYear) await setSchoolYear(schoolYear);
+	if (teachers) await setTeacher(teachers);
+	if (classGrade) await setClassGrade(classGrade);
+	if (className) await setClassName(className);
+	if (customClassName) await setCustomClassName(customClassName);
+	await clickEditClassSaveChangesBtn();
 }
 
-async function createNewClass({schoolYear, teachers, classGrade, className, customClassName}) {
-    await manageClassesPage.clickCreateClassBtn();
-    await clickMoreOptionsBtn();
-    if (schoolYear) await setSchoolYear(schoolYear);
-    if (teachers) await setTeachers(teachers);
-    if (classGrade) await setClassGrade(classGrade);
-    if (className) await setClassName(className);
-    if (customClassName) await setCustomClassName(customClassName);
-    await clickAddClassConfirmation();
+async function createNewClass({ schoolYear, teachers, classGrade, className, customClassName }) {
+	await manageClassesPage.clickCreateClassBtn();
+	await clickMoreOptionsBtn();
+	if (schoolYear) await setSchoolYear(schoolYear);
+	if (teachers) await setTeachers(teachers);
+	if (classGrade) await setClassGrade(classGrade);
+	if (className) await setClassName(className);
+	if (customClassName) await setCustomClassName(customClassName);
+	await clickAddClassConfirmation();
 }
 async function isClassEdited(newClassName, teacherLastname, expectedResult) {
 	if (!expectedResult) {
 		try {
-			const allClassesContainer = await waitHelpers.waitUntilElementIsVisible(classListTable)
-			const allClassesContent = await allClassesContainer.getText()
-			const contentArray = allClassesContent.split(" ")
-			expect(contentArray[0]).not.to.equal(newClassName)
-			expect(contentArray[1]).not.to.equal(teacherLastname)
+			const allClassesContainer = await waitHelpers.waitUntilElementIsVisible(classListTable);
+			const allClassesContent = await allClassesContainer.getText();
+			const contentArray = allClassesContent.split(' ');
+			expect(contentArray[0]).not.to.equal(newClassName);
+			expect(contentArray[1]).not.to.equal(teacherLastname);
 		} catch (e) {
-			return true
+			return true;
 		}
 	} else {
 		try {
-			const allClassesContainer = await waitHelpers.waitUntilElementIsVisible(classListTable)
-			const allClassesContent = await allClassesContainer.getText()
-			const contentArray = allClassesContent.split(" ")
-			expect(contentArray.length).to.equal(4) // teacher is not empty and therefore not 3, but 4
-			expect(contentArray[0]).to.equal(newClassName)
-			expect(contentArray[1]).to.equal(teacherLastname)
+			const allClassesContainer = await waitHelpers.waitUntilElementIsVisible(classListTable);
+			const allClassesContent = await allClassesContainer.getText();
+			const contentArray = allClassesContent.split(' ');
+			expect(contentArray.length).to.equal(4); // teacher is not empty and therefore not 3, but 4
+			expect(contentArray[0]).to.equal(newClassName);
+			expect(contentArray[1]).to.equal(teacherLastname);
 		} catch (e) {
-			return false
+			return false;
 		}
 	}
-
 }
 async function deleteClass() {
 	await elementHelpers.click(deleteClassBtn);
 	await elementHelpers.clickAndWait(deleteButtonConfirmation);
 }
 
-async function isNewClassCreated(className, numOfStudents) {
-    const allClassesContainer = await waitHelpers.waitUntilElementIsVisible(classListTable)
-    const allClassesContent = await allClassesContainer.getText()
-    const contentArray = allClassesContent.split(" ")
-    const currentYear = new Date().getFullYear().toString().substring(2) // 20
-
-    expect(contentArray.length).to.equal(3) // teacher column should be empty and therefore not 4, but 3
-    expect(contentArray[0]).to.equal(className)
-    expect(contentArray[1].includes(currentYear)).to.equal(true)
-    expect(contentArray[2]).to.equal(numOfStudents)
-}
-
-
 module.exports = {
-    editClass,
-    createNewClass,
-    isClassEdited,
-    isNewClassCreated,
-    deleteClass,
-    setStudent,
-    createListOfStudents,
-    setTeacher,
-    clickEditClassSaveChangesBtn,
-    deleteStudentFromClass,
-    deleteTeacherFromClass
-}
+	editClass,
+	createNewClass,
+	isClassEdited,
+	deleteClass,
+	setStudent,
+	createListOfStudents,
+	setTeacher,
+	clickEditClassSaveChangesBtn,
+	deleteStudentFromClass,
+	deleteTeacherFromClass,
+};
