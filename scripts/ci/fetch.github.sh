d10b3394695cad4afb7d8023e965550fd9894e90
mamutmk5 <paul.poppe@hpi.de>
2021-08-16 18:16:43 +0200

    Update fetch.github.sh

--- scripts/ci/fetch.github.sh
+++ scripts/ci/fetch.github.sh
@@ -27,20 +27,15 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongod
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl git mongodb-org-tools
 
-# set envs
-if [[ -z "$BRANCH_NAME" ]]; then
-    echo "Must provide BRANCH_NAME in environment"
-    exit 1
-fi
-echo "BRANCH: $BRANCH_NAME"
+echo "BRANCH: ${{ env.BRANCH_NAME }}"
 
 # fetch default (main) script
 echo "try fetching script from default branch"
 curl -fO "https://raw.githubusercontent.com/hpi-schul-cloud/end-to-end-tests/main/scripts/ci/end-to-end-tests.github.sh" || true
 
 # use branch specific script if available
-echo "try fetching script from $BRANCH_NAME branch"
-curl -fO "https://raw.githubusercontent.com/hpi-schul-cloud/end-to-end-tests/$BRANCH_NAME/scripts/ci/end-to-end-tests.github.sh" || true
+echo "try fetching script from ${{ env.BRANCH_NAME }} branch"
+curl -fO "https://raw.githubusercontent.com/hpi-schul-cloud/end-to-end-tests/${{ env.BRANCH_NAME }}/scripts/ci/end-to-end-tests.github.sh" || true
 
 ls -a
 chmod 700 end-to-end-tests.github.sh
