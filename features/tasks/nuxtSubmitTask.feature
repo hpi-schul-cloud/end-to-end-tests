3f767de5fcaa6f5d2fb395bafac6a71c9fa8056f
Khan <KhanUz@fhhnet.stadt.hamburg.de>
2021-09-08 13:04:13 +0200

    Update the step functions

--- features/tasks/nuxtSubmitTask.feature
+++ features/tasks/nuxtSubmitTask.feature
@@ -7,15 +7,15 @@ Feature: View the task in new nuxt task menu and submit it
     @nuxtSubmitAndViewTask @e2eCore
     Scenario Outline: As a user, I want to see the open tasks in new nuxt tab, sumbit them and see it will show at Complete task tab
         When <userRole> logs in
-        And clicks the current task tab
-        And click on without duedate task section
-        And click to the task
-        Then click to the Abgabe tab
-        And sets submission text 'Test submission text'
-        And click on submit button
-        And click again to current task in side navigation menu
-        And click to the completed task tab
-        Then click at ungraded task
+        And <userRole> clicks the current task tab
+        And <userRole> clicks on without duedate task section
+        And <userRole> clicks task
+        And <userRole> clicks on Submission tab
+        And <userRole> sets submission text 'Test submission text'
+        And <userRole> clicks on submit button
+        And <userRole> clicks the current task tab
+        And <userRole> clicks completed task tab
+        Then <userRole> clicks at ungraded task
         Examples:
 			| userRole |
 			| student  |
--- step_definitions/tasks/nuxtSubmitTask_steps.js
+++ step_definitions/tasks/nuxtSubmitTask_steps.js
@@ -5,39 +5,26 @@ const TASKPage = require("../../page-objects/pages/TASKPage");
 const TASKListPage = require("../../page-objects/pages/TASKListPage");
 
 
-When(/^.*clicks the current task tab$/, async function (){
+When(/^.* clicks the current task tab$/, async function (){
     await TASKPage.gotoNuxtTasksTab();
 });
 
-When(/^.*click on without duedate task section$/, async function (){
+When(/^.* clicks on without duedate task section$/, async function (){
     await TASKPage.taskWithoutDueDate();
 });
 
-When(/^.*click to the task$/, async function (){
+When(/^.* clicks task$/, async function (){
     await TASKListPage.clickTaskWithoutDuedate();
 });
 
-//THEN
-Then(/^.*click to the Abgabe tab$/, async function () {
-    await TASKPage.clickStudentSubmissionTab();
-});
-
-When(/^.*sets submission text '(.*)'$/, async function (submissionText) {
-    await TASKPage.setTextSubmision(submissionText);
-});
-
-When(/^.*click on submit button$/, async function (){
+When(/^.* clicks on submit button$/, async function (){
     await TASKPage.clickSaveAndSendSubmissionBtn();
 });
 
-When(/^.*click again to current task in side navigation menu$/, async function (){
-    await TASKPage.gotoNuxtTasksTab();
-});
-
-When(/^.*click to the completed task tab$/, async function (){
+When(/^.* clicks completed task tab$/, async function (){
     await TASKPage.clickCompletedTab();
 });
 
-When(/^.*click at ungraded task$/, async function (){
+When(/^.* clicks at ungraded task$/, async function (){
     await TASKListPage.clickUngradedTask();
 });
