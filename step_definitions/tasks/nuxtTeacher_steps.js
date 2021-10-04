53c0bcdceb855e9fb692d4f23123b17ae1e946b9
Uzaeir <86830022+Uzaeir@users.noreply.github.com>
2021-10-04 13:52:22 +0200

    Typo updated in studentSubmittedTask

--- step_definitions/tasks/nuxtTeacher_steps.js
+++ step_definitions/tasks/nuxtTeacher_steps.js
@@ -5,7 +5,7 @@ const TASKPage = require("../../page-objects/pages/TASKPage");
 const TASKListPage = require("../../page-objects/pages/TASKListPage");
 
 When(/^.* clicks on student submitted the task$/, async function (){
-    await TASKListPage.studentSubmittedtTask();
+    await TASKListPage.studentSubmittedTask();
 });
 
 When(/^.* student graded for Task19$/, async function(){
