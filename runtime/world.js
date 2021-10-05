54006cfe3c1e198554bc6701ec6a231d1aa44456
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-10-05 15:28:34 +0200

    report axios result

--- runtime/world.js
+++ runtime/world.js
@@ -50,7 +50,7 @@ global.log = log;
 /**
  * This is the Global date functionality
  */
-global.date = require('./helpers/dateTimeHelpers').getCurrentDateWithFormat("dd-mm-yyyy");
+global.date = require('./helpers/dateTimeHelpers').getCurrentDateWithFormat('dd-mm-yyyy');
 
 /**
  * for all API test calls
@@ -94,10 +94,7 @@ async function getDriverInstance() {
 	let options = {};
 	if (remoteService && remoteService.type === 'browserstack') {
 		let configType = global.settings.remoteConfig;
-		assert.isString(
-			configType,
-			'BrowserStack requires a config type e.g. win10-chrome'
-		);
+		assert.isString(configType, 'BrowserStack requires a config type e.g. win10-chrome');
 		driver = BrowserStackDriver(options, configType);
 		return driver;
 	}
@@ -107,12 +104,12 @@ async function getDriverInstance() {
 			{
 				driver = FirefoxDriver(options);
 			}
-		break;
+			break;
 		case 'chrome':
 			{
 				driver = ChromeDriver(options);
 			}
-		break;
+			break;
 	}
 	return driver;
 }
@@ -146,16 +143,7 @@ function consoleInfo() {
  * All Global variables
  * @constructor
  */
-const {
-	Before,
-	After,
-	AfterAll,
-	Status,
-	Given,
-	And,
-	When,
-	Then
-} = require('@cucumber/cucumber');
+const { Before, After, AfterAll, Status, Given, And, When, Then } = require('@cucumber/cucumber');
 
 global.Given = Given;
 global.When = When;
@@ -164,10 +152,10 @@ global.And = And;
 
 function World() {
 	/**
-	* create a list of variables to expose globally and therefore accessible within each step definition
-	* @type {{driver: null, webdriverio, webdrivercss: *, expect: *, assert: (*), trace: consoleInfo,
-	* log: log, page: {}, shared: {}}}
-	*/
+	 * create a list of variables to expose globally and therefore accessible within each step definition
+	 * @type {{driver: null, webdriverio, webdrivercss: *, expect: *, assert: (*), trace: consoleInfo,
+	 * log: log, page: {}, shared: {}}}
+	 */
 	let runtime = {
 		driver: {}, // the browser object
 		expect: global.expect, // expose chai expect to allow variable testing
@@ -181,32 +169,32 @@ function World() {
 		envConfig: global.envConfig, // expose the global environment configuration file for use when changing environment // types (i.e. dev, test, preprod)
 		downloader: global.downloader, // exposes the downloader for global usage
 		request: global.request, // exposes the request-promise for API testing
-		date: global.date // expose the date method for logs and reports
+		date: global.date, // expose the date method for logs and reports
 	};
 	/**
-	*  expose properties to step definition methods via global variables
-	*/
-	Object.keys(runtime).forEach(function(key) {
+	 *  expose properties to step definition methods via global variables
+	 */
+	Object.keys(runtime).forEach(function (key) {
 		/** make property/method available as a global (no this. prefix required)
-		*/
+		 */
 		global[key] = runtime[key];
 	});
 	/**
-	* import page objects (after global lets have been created)
-	*/
+	 * import page objects (after global lets have been created)
+	 */
 	if (global.paths.pageObjects && fs.existsSync(global.paths.pageObjects)) {
 		/** require all page objects using camelcase as object names
-		*/
+		 */
 		runtime.page = requireDir(global.paths.pageObjects, { camelcase: true });
 		/**
-		* expose globally
-		* @type {{}}
-		*/
+		 * expose globally
+		 * @type {{}}
+		 */
 		global.page = runtime.page;
 	}
 	/**
-	* import shared objects from multiple paths (after global lets have been created)
-	*/
+	 * import shared objects from multiple paths (after global lets have been created)
+	 */
 	if (
 		global.paths.sharedObjects &&
 		Array.isArray(global.paths.sharedObjects) &&
@@ -214,20 +202,20 @@ function World() {
 	) {
 		let allDirs = {};
 		/**
-		* first require directories into objects by directory
-		*/
-		global.paths.sharedObjects.forEach(function(itemPath) {
+		 * first require directories into objects by directory
+		 */
+		global.paths.sharedObjects.forEach(function (itemPath) {
 			if (fs.existsSync(itemPath)) {
 				let dir = requireDir(itemPath, { camelcase: true });
 				merge(allDirs, dir);
 			}
 		});
 		/** if we managed to import some directories, expose them
-		*/
+		 */
 		if (Object.keys(allDirs).length > 0) {
 			/** expose globally
-			* @type {{}}
-			*/
+			 * @type {{}}
+			 */
 			global.shared = allDirs;
 		}
 	}
@@ -253,7 +241,7 @@ const cucumberTimeout = process.env.CUCUMBER_TIMEOUT || 60000;
 setDefaultTimeout(cucumberTimeout);
 
 // start recording of the Test run time
-global.startDateTime = dateTimeHelpers.getCurrentDateWithFormat("dd/MM/yyyy HH:mm:ss");
+global.startDateTime = dateTimeHelpers.getCurrentDateWithFormat('dd/MM/yyyy HH:mm:ss');
 
 /**
  * create the driver before scenario if it's not instantiated
@@ -277,7 +265,7 @@ Before(async function (scenario) {
 	}
 	console.log('\n\nResetting the DB...');
 	const output = await axios.post('http://localhost:3333/api/management/database/seed');
-	console.log('Done.');
+	console.log('Done:', output.data);
 	return;
 });
 
@@ -287,7 +275,7 @@ Before(async function (scenario) {
 AfterAll(async () => {
 	let driver = global.driver;
 	if (program.email) {
-		driver.pause(DELAY_3_SECOND).then(function() {
+		driver.pause(DELAY_3_SECOND).then(function () {
 			return emailHelpers.klassiEmail();
 		});
 	}
@@ -296,10 +284,10 @@ AfterAll(async () => {
 /**
  * compile and generate a report at the END of the test run and send an Email
  */
-AfterAll(function(done) {
+AfterAll(function (done) {
 	let driver = global.driver;
 	if (global.paths.reports && fs.existsSync(global.paths.reports)) {
-		global.endDateTime = dateTimeHelpers.getCurrentDateWithFormat("dd/MM/yyyy HH:mm:ss");
+		global.endDateTime = dateTimeHelpers.getCurrentDateWithFormat('dd/MM/yyyy HH:mm:ss');
 		let reportOptions = {
 			theme: 'bootstrap',
 			jsonFile: path.resolve(
@@ -319,15 +307,12 @@ AfterAll(function(done) {
 				Platform: process.platform,
 				'Test Environment': process.env.NODE_ENV || 'DEVELOPMENT',
 				Browser: global.settings.remoteConfig || global.browserName,
-				Executed:
-					remoteService && remoteService.type === 'browserstack'
-						? 'Remote'
-						: 'Local'
+				Executed: remoteService && remoteService.type === 'browserstack' ? 'Remote' : 'Local',
 			},
 			brandTitle: reportName + '-' + global.endDateTime,
-			name: projectName
+			name: projectName,
 		};
-		driver.pause(DELAY_3_SECOND).then(function() {
+		driver.pause(DELAY_3_SECOND).then(function () {
 			reporter.generate(reportOptions);
 			driver.pause(DELAY_3_SECOND);
 		});
@@ -338,12 +323,12 @@ AfterAll(function(done) {
 /**
  *  executed after each scenario (always closes the browser to ensure fresh tests)
  */
-After(async function(scenario) {
+After(async function (scenario) {
 	let driver = global.driver;
 	if (scenario.result.status === Status.FAILED) {
 		if (remoteService && remoteService.type === 'browserstack') {
 			await driver.deleteSession();
-		} else if(!global.settings.keepOpenOnError){
+		} else if (!global.settings.keepOpenOnError) {
 			// Comment out to do nothing | leave browser open
 			await driver.deleteSession();
 		}
@@ -351,20 +336,20 @@ After(async function(scenario) {
 		if (remoteService && remoteService.type !== 'browserstack') {
 			// Comment out to do nothing | leave browser open
 			await driver.deleteSession();
-		} else if(!global.settings.keepOpenOnError){
+		} else if (!global.settings.keepOpenOnError) {
 			await driver.deleteSession();
-		}	
+		}
 	}
 });
 
 /**
  * get executed only if there is an error within a scenario
  */
-After(function(scenario) {
+After(function (scenario) {
 	let driver = global.driver;
 	let world = this;
 	if (scenario.result.status === Status.FAILED) {
-		return driver.takeScreenshot().then(function(screenShot) {
+		return driver.takeScreenshot().then(function (screenShot) {
 			world.attach(screenShot, 'image/png');
 		});
 	}
