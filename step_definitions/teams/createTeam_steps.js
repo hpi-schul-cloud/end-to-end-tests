877e4dc8960970d02b5467cd8214a07346c31c09
yuliashikhareva <yulia.shikhareva@gmail.com>
2021-10-10 23:06:37 +0200

    Revert "remove async in step-definitions"
    This reverts commit abf3824137ce7d5cf7a72349d322163c64353c8b.

--- step_definitions/teams/createTeam_steps.js
+++ step_definitions/teams/createTeam_steps.js
@@ -5,7 +5,7 @@ const TMSTeamListPage = require('../../page-objects/pages/teamsPages/TMSTeamList
 const TMSTeamMembersPage = require('../../page-objects/pages/teamsPages/TMSTeamMembersPage.js');
 
 //WHEN
-When(/^.*creates a new team with name '([^']*)' and description '([^']*)' and color '([^']*)'$/, (teamName, description, colour) => {
+When(/^.*creates a new team with name '([^']*)' and description '([^']*)' and color '([^']*)'$/, async function (teamName, description, colour) {
 	await TMSAddEditTeamPage.createTeamWithName(teamName, description, colour);
 });
 
@@ -13,61 +13,64 @@ When(/^.*goes too the team member settings$/, async function () {
 	await TMSAddEditTeamPage.goToTeamMembersSettings();
 });
 
-When(/^.*adds a student with lastname: '([^']*)' and firstname: '([^']*)' to the team$/, (lastname, firstname) => {
+When(/^.*adds a student with lastname: '([^']*)' and firstname: '([^']*)' to the team$/, async function (lastname, firstname) {
 	await TMSTeamMembersPage.addTeamAttendee(lastname, firstname);
 });
 
-When(/^.*clicks Submit-add-team-member button$/, () => {
+When(/^.*clicks Submit-add-team-member button$/, async function () {
 	await TMSTeamMembersPage.clickSubmitAddTeamAttendeeBtn();
 });
 
-When(/^.*clicks on Member icon in team with name '([^']*)'$/, (teamName) => {
+When(/^.*clicks on Member icon in team with name '([^']*)'$/, async function (teamName) {
 	await TMSTeamListPage.clickMemberIconInTeam(teamName);
 });
 
 //THEN
-Then(/^.*team with name '([^']*)' is be visible on the list$/, (teamName) => {
+Then(/^.*team with name '([^']*)' is be visible on the list$/, async function (teamName) {
 	await TMSTeamListPage.isTeamVisible(teamName, true);
 });
 
-Then(/^.*team with name '([^']*)' has colour '([^']*)'$/, (teamName, teamColour) => {
+Then(/^.*team with name '([^']*)' has colour '([^']*)'$/, async function (teamName, teamColour) {
 	await TMSTeamListPage.isTeamColour(teamName, teamColour);
 });
 
-Then(/^.*team with name '([^']*)' member number is '([^']*)'$/, (teamName, memberCount) => {
+Then(/^.*team with name '([^']*)' member number is '([^']*)'$/, async function (teamName, memberCount) {
 	await TMSTeamListPage.isTeamMemberNumber(teamName, memberCount);
 });
 
-Then(/^.*team with name '([^']*)', colour '([^']*)' and  member number '([^']*)' is visible on the list$/,(teamName, teamColour, memberCount) => {
+Then(/^.*team with name '([^']*)', colour '([^']*)' and  member number '([^']*)' is visible on the list$/, async function (teamName, teamColour, memberCount) {
 	await TMSTeamListPage.isTeamVisible(teamName, true);
 	await TMSTeamListPage.isTeamColour(teamName, teamColour);
 	await TMSTeamListPage.isTeamMemberNumber(teamName, memberCount);
 });
 
-Then(/^.*team members: '([^']*)' are listed$/, (listOfMembers) => {
+Then(/^.*team members: '([^']*)' are listed$/,  async function (listOfMembers) {
 	await TMSTeamListPage.areTeamMembersOnTheList(listOfMembers, true);
 });
 
-Then(/^.*team members: '([^']*)' are not listed$/,(listOfMembers) => {
+Then(/^.*team members: '([^']*)' are not listed$/,  async function (listOfMembers) {
 	await TMSTeamListPage.areTeamMembersOnTheList(listOfMembers, false);
 });
 
-Then(/^.* goes to Teams Page$/, () => {
+Then(/^.* goes to Teams Page$/, async function () {
 	await TMSTeamListPage.goToTeams();
 });
 
-When(/^.* chooses team with name '([^']*)'$/, (teamName) => {
+When(/^.* chooses team with name '([^']*)'$/, function (teamName) {
 	return TMSTeamListPage.clickOnChosenTeam(teamName);
 });
 
-Then(/^.* team with name '([^']*)' is visible on the list$/, (teamName) => {
+Then(/^.* team with name '([^']*)' is visible on the list$/, async function (teamName) {
 	return TMSTeamListPage.isTeamVisible(teamName, true);
 });
 
-When(/^.* chooses team colour '([^']*)'$/, (teamColour) => {
+When(/^.* chooses team colour '([^']*)'$/, function (teamColour) {
 	return TMSAddEditTeamPage.setTeamColour(teamColour);
 });
 
-Then(/^.* team name '([^']*)' with description correctly displayed '([^']*)'$/, (teamName, expectedDescription) => {
+Then(/^.* team name '([^']*)' with description correctly displayed '([^']*)'$/, async function (
+	teamName,
+	expectedDescription
+) {
 	await TMSTeamListPage.isTeamDescription(teamName, expectedDescription);
 });
