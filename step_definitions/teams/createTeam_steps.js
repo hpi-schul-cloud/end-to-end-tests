4354fed818df1388def1b3141389f72513cc7fe6
yuliashikhareva <yulia.shikhareva@gmail.com>
2021-10-10 22:47:13 +0200

    Update createTeam_steps.js

--- step_definitions/teams/createTeam_steps.js
+++ step_definitions/teams/createTeam_steps.js
@@ -6,54 +6,54 @@ const TMSTeamMembersPage = require('../../page-objects/pages/teamsPages/TMSTeamM
 
 //WHEN
 When(/^.*creates a new team with name '([^']*)' and description '([^']*)' and color '([^']*)'$/, (teamName, description, colour) => {
-	await TMSAddEditTeamPage.createTeamWithName(teamName, description, colour);
+	TMSAddEditTeamPage.createTeamWithName(teamName, description, colour);
 });
 
 When(/^.*goes too the team member settings$/, async function () {
-	await TMSAddEditTeamPage.goToTeamMembersSettings();
+	TMSAddEditTeamPage.goToTeamMembersSettings();
 });
 
 When(/^.*adds a student with lastname: '([^']*)' and firstname: '([^']*)' to the team$/, (lastname, firstname) => {
-	await TMSTeamMembersPage.addTeamAttendee(lastname, firstname);
+	TMSTeamMembersPage.addTeamAttendee(lastname, firstname);
 });
 
 When(/^.*clicks Submit-add-team-member button$/, () => {
-	await TMSTeamMembersPage.clickSubmitAddTeamAttendeeBtn();
+	 TMSTeamMembersPage.clickSubmitAddTeamAttendeeBtn();
 });
 
 When(/^.*clicks on Member icon in team with name '([^']*)'$/, (teamName) => {
-	await TMSTeamListPage.clickMemberIconInTeam(teamName);
+	TMSTeamListPage.clickMemberIconInTeam(teamName);
 });
 
 //THEN
 Then(/^.*team with name '([^']*)' is be visible on the list$/, (teamName) => {
-	await TMSTeamListPage.isTeamVisible(teamName, true);
+	TMSTeamListPage.isTeamVisible(teamName, true);
 });
 
 Then(/^.*team with name '([^']*)' has colour '([^']*)'$/, (teamName, teamColour) => {
-	await TMSTeamListPage.isTeamColour(teamName, teamColour);
+	TMSTeamListPage.isTeamColour(teamName, teamColour);
 });
 
 Then(/^.*team with name '([^']*)' member number is '([^']*)'$/, (teamName, memberCount) => {
-	await TMSTeamListPage.isTeamMemberNumber(teamName, memberCount);
+	TMSTeamListPage.isTeamMemberNumber(teamName, memberCount);
 });
 
 Then(/^.*team with name '([^']*)', colour '([^']*)' and  member number '([^']*)' is visible on the list$/,(teamName, teamColour, memberCount) => {
-	await TMSTeamListPage.isTeamVisible(teamName, true);
-	await TMSTeamListPage.isTeamColour(teamName, teamColour);
-	await TMSTeamListPage.isTeamMemberNumber(teamName, memberCount);
+	 TMSTeamListPage.isTeamVisible(teamName, true);
+	 TMSTeamListPage.isTeamColour(teamName, teamColour);
+	 TMSTeamListPage.isTeamMemberNumber(teamName, memberCount);
 });
 
 Then(/^.*team members: '([^']*)' are listed$/, (listOfMembers) => {
-	await TMSTeamListPage.areTeamMembersOnTheList(listOfMembers, true);
+	TMSTeamListPage.areTeamMembersOnTheList(listOfMembers, true);
 });
 
 Then(/^.*team members: '([^']*)' are not listed$/,(listOfMembers) => {
-	await TMSTeamListPage.areTeamMembersOnTheList(listOfMembers, false);
+	TMSTeamListPage.areTeamMembersOnTheList(listOfMembers, false);
 });
 
 Then(/^.* goes to Teams Page$/, () => {
-	await TMSTeamListPage.goToTeams();
+	TMSTeamListPage.goToTeams();
 });
 
 When(/^.* chooses team with name '([^']*)'$/, (teamName) => {
@@ -69,5 +69,5 @@ When(/^.* chooses team colour '([^']*)'$/, (teamColour) => {
 });
 
 Then(/^.* team name '([^']*)' with description correctly displayed '([^']*)'$/, (teamName, expectedDescription) => {
-	await TMSTeamListPage.isTeamDescription(teamName, expectedDescription);
+	TMSTeamListPage.isTeamDescription(teamName, expectedDescription);
 });
