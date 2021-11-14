0967c3b036f8629419669e1a4071283f7992ef40
yuliashikhareva <yulia.shikhareva@gmail.com>
2021-11-15 00:04:23 +0100

    repair broken scenario @completedTaskTab

--- page-objects/pages/TASKPage.js
+++ page-objects/pages/TASKPage.js
@@ -20,7 +20,8 @@ const ratingViewSel = '.grade';
 const remarkViewSel = '.ckcontent.comment';
 const submissionsTable = '#submissions table';
 const submissionRow = `${submissionsTable} tbody tr.userinfo`;
-const completedTaskTab = "//*[text()='Erledigte Aufgaben']";
+//const completedTaskTab = "//*[text()='Erledigte Aufgaben']";
+const completedTaskTab = '[data-testid="closedTasks"]'
 const gradedTask = "//div[@class='v-list-item__title' and text() = '1']";
 let fileUrl;
 
