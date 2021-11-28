988f7128242c5857eb64571784fcfe2168ac0600
yuliashikhareva <yulia.shikhareva@gmail.com>
2021-11-28 23:59:02 +0100

    Update servers.js

--- shared-objects/servers.js
+++ shared-objects/servers.js
@@ -7,8 +7,8 @@ const config = {
 		HOST: process.env.IT_CLIENT_HOST || 'localhost',
 		PORT: process.env.IT_CLIENT_PORT || '4000',
 		get URL() {
-			return 'https://nbc-bc-485.cd.dbildungscloud.dev/login/';
-			//return `${config.CLIENT.PROTOCOLL}://${config.CLIENT.HOST}:${config.CLIENT.PORT}`;
+			//return 'https://nbc-bc-485-yulia.cd.dbildungscloud.dev';
+			return `${config.CLIENT.PROTOCOLL}://${config.CLIENT.HOST}:${config.CLIENT.PORT}`;
 		},
 	},
 	SERVER: {
@@ -16,8 +16,8 @@ const config = {
 		HOST: process.env.IT_SERVER_HOST || 'localhost',
 		PORT: process.env.IT_SERVER_PORT || '3030',
 		get URL() {
-			//return `${config.SERVER.PROTOCOLL}://${config.SERVER.HOST}:${config.SERVER.PORT}`;
-			return 'https://nbc-bc-485.cd.dbildungscloud.dev/api/v1'
+			return `${config.SERVER.PROTOCOLL}://${config.SERVER.HOST}:${config.SERVER.PORT}`;
+			//return 'https://nbc-bc-485-yulia.cd.dbildungscloud.dev/api/v1'
 		},
 	},
 	MANAGEMENT_SERVER: {
@@ -25,7 +25,7 @@ const config = {
 		HOST: process.env.IT_MANAGEMENT_SERVER_HOST || 'localhost',
 		PORT: process.env.IT_MANAGEMENT_SERVER_PORT || '3333',
 		get URL() {
-			return 'http://mgmt-svc.nbc-bc-485.svc.cluster.local:3333/api/management/database/seed';
+			return 'http://mgmt-svc.nbc-bc-485-yulia.svc.cluster.local:3333/api/management/database/seed';
 		},
 	},
 };
