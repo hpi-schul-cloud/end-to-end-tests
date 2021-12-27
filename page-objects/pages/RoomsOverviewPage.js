8a9b2faa57a039172048c2882d46fdacc61a211e
yuliashikhareva <yulia.shikhareva@gmail.com>
2021-12-28 00:46:04 +0100

    Update RoomsOverviewPage.js

--- page-objects/pages/RoomsOverviewPage.js
+++ page-objects/pages/RoomsOverviewPage.js
@@ -3,9 +3,11 @@
 const elementHelpers = require('../../runtime/helpers/elementHelpers');
 const navigationLeftPage = require('./NavigationLeftPage');
 
+const elementsContainer = ".rooms-container"
 const rowsSelector = '.room-overview-row'
 const columsSelector = ".room-overview-col"
 const nameOfTheCourseSelector = ".justify-center.mt-1.sub-title"
+
 
 async function goToRoomsOverview() {
 	await navigationLeftPage.loadNavItemRoomsOverview();
@@ -16,14 +18,14 @@ async function getListOfElementsRoomsOverview() {
     const rows = await getNumberOfRowsRoomsOverview();
     let coursesOnThePage = [];
     for (var i=0; i<=rows; i++) {
-        let currentRow = `${rowsSelector}: nth-child(${i})`;
+        let currentRow = `${rowsSelector}:nth-child(${i})`;
         let columns = await driver.$$(`${currentRow} > ${columsSelector}`);
         for (var j=0; j<=columns; j++ ) {
-            let nameOfCurrentElementSel = `${rowsSelector}:nth-child(${i}) > div:nth-child(${j}) > ${nameOfTheCourseSelector}`;
-            let nameOfCurrentElement = await nameOfCurrentElementSel.getText();
+            let nameOfCurrentElementSel = await driver.$(`${rowsSelector}:nth-child(${i}) > div:nth-child(${j}) > ${nameOfTheCourseSelector}`);
+            let nameOfCurrentElement = await nameOfCurrentElementSel.getValue();
             coursesOnThePage.push(nameOfCurrentElement)
         }
-    }
+    } 
     return coursesOnThePage;
 }
 
@@ -35,7 +37,8 @@ async function isCourseNameDisplayedOnTheList(name) {
 /* private helper method which counts the number of rows on the page and therefore the number of "subcontainers" of the elements */
 
 async function getNumberOfRowsRoomsOverview() {
-	await elementHelpers.getListOfAllElements(rowsSelector);
+    let numberOfRowsRoomsOverview = await driver.$$(rowsSelector);
+    return numberOfRowsRoomsOverview;
 }
 
 /* to be commented in after implementation of rooms-overview logic
