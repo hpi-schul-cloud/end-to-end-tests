71810534dc04e0ffcef9db6d62ead3db6492dc19
UzaeirDP <uzair.khan@dataport.de>
2022-02-22 10:16:18 +0100

    Update elementHelpers.js

--- runtime/helpers/elementHelpers.js
+++ runtime/helpers/elementHelpers.js
@@ -268,7 +268,7 @@ function SelectorConflictException (message) {
 }
 
 async function hoverOverMenuOptions(selector){
-	await driver.pause(5000);
+	await waitHelpers.waitUntilElementIsClickable(selector);
 	let hoverOverMenuOptions = await driver.$(selector);
 	await scrollToElement(hoverOverMenuOptions);
 	await click(selector);
