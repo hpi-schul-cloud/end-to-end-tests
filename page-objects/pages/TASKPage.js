a8d74ef8e31e63dc7ebab43f3f9f1ea5c9667448
UzaeirDP <uzair.khan@dataport.de>
2022-06-21 16:27:19 +0200

    Update TASKPage.js

--- page-objects/pages/TASKPage.js
+++ page-objects/pages/TASKPage.js
@@ -16,7 +16,7 @@ const studentSubmissionTab = "//a[@id='submission-tab-link' and contains(.,  'Ab
 const remoteFilePathInput = 'input[type=file][class=dz-hidden-input]';
 const commentBtn = '#comment-tab-link';
 const commentGradingTabSel = '#feedback-tab-link';
-const createTaskAndTopicBtn = "//div[@data-testid='add-content-button']";
+const createTaskAndTopicBtn = "//button[@aria-label = 'Erstellen']";
 const taskRatingInput = '[data-testid="evaluation_procent"]';
 const ratingViewSel = '.grade';
 const remarkViewSel = '.ckcontent.comment';
