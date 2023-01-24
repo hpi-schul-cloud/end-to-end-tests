1e1d104aa0a7ee4dd39cfddf7d400bfcd4920d75
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2023-01-24 10:42:41 +0100

    remove debug code from script

--- scripts/ci/end-to-end-tests.github.sh
+++ scripts/ci/end-to-end-tests.github.sh
@@ -93,18 +93,18 @@ before(){
 	echo "INSTALL DEPENDNECIES..."
 	cd end-to-end-tests && npm ci && cd ..
 
-	echo "1. ls"
-	echo $NODE_ENV
-	ls
+	# echo "1. ls"
+	# echo $NODE_ENV
+	# ls
 	cd nuxt-client
-	echo "2. ls"
+	# echo "2. ls"
 	rm package.json package-lock.json
-	npm cache clean --force
-	npm install http-proxy-middleware -dd
-	npm install express
-	echo "3. ls"
-	npm ls
-	ls
+	# npm cache clean --force
+	npm install express http-proxy-middleware
+	# npm install express
+	# echo "3. ls"
+	# npm ls
+	# ls
 	node server-proxy.js &
 	cd ..
 
