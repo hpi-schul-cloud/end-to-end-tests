468647e6b7ef250c023ab258fe5bd2fff71aeace
davwas <daw.wasik@gmail.com>
2024-04-16 12:13:06 +0200

    change installed package 2

--- scripts/ci/fetch.github.sh
+++ scripts/ci/fetch.github.sh
@@ -25,7 +25,7 @@ set -e
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv B00A0BD1E2C63C11
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl git mongodb-org
+sudo apt install -y apt-transport-https ca-certificates curl git mongodb-org-tools
 
 if [[ -z "$BRANCH_NAME" ]]; then
     echo "Must provide BRANCH_NAME in environment"
