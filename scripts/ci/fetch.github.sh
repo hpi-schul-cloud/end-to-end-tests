c8322b662ddbc1bcca27ef756f8e9f157af494fc
virgilchiriac <virgil.chiriac@dataport.de>
2024-07-05 18:03:55 +0200

    install public key for mongo

--- scripts/ci/fetch.github.sh
+++ scripts/ci/fetch.github.sh
@@ -22,7 +22,8 @@
 set -e
 
 # install dependencies
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv B00A0BD1E2C63C11
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv B00A0BD1E2C63C11
+wget -qO - https://pgp.mongodb.com/server-6.0.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl git mongodb-database-tools
