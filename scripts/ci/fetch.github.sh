c4416ed5ac10b0e1e8d65cdb322df8dec91fbc78
virgilchiriac <17074330+virgilchiriac@users.noreply.github.com>
2024-07-11 13:54:21 +0200

    BC-7461 - upgrade mongodb to v6 on ubuntu installation (#455)
    * upgrade mongodb to v6 on ubuntu installation
    
    * install public key for mongo
    
    * remove mongo install on base machine
    
    we have it via docker-compose, install mongo here is not needed
    
    ---------
    
    Co-authored-by: MartinSchuhmacher <mschuhmacher@gmx.de>

--- scripts/ci/fetch.github.sh
+++ scripts/ci/fetch.github.sh
@@ -22,10 +22,8 @@
 set -e
 
 # install dependencies
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv B00A0BD1E2C63C11
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl git mongodb-database-tools
+sudo apt-get -y install gnupg apt-transport-https ca-certificates curl git
+sudo apt-get update
 
 if [[ -z "$BRANCH_NAME" ]]; then
     echo "Must provide BRANCH_NAME in environment"
